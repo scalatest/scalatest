f77ed5b58ace0063ebaf1eb60e84f53faffc5e15
bvenners <bill@artima.com>
2013-06-03 19:40:05 -0700

    Cleaned up the Scaladoc on LoneElement.

--- src/main/scala/org/scalatest/LoneElement.scala
+++ src/main/scala/org/scalatest/LoneElement.scala
@@ -4,46 +4,84 @@ import collection.GenTraversable
 import matchers.ShouldMatchers.TraversableShouldWrapper
 
 /**
- * This class is part of the ScalaTest matchers DSL. Please see the documentation for <a href="ShouldMatchers.html"><code>ShouldMatchers</code></a> or <a href="MustMatchers.html"><code>MustMatchers</code></a> for an overview of
- * the matchers DSL.
+ * Trait that provides an implicit conversion that adds a <code>loneElement</code> method
+ * to <code>GenTraversable</code>, which will return the value of the lone element if the collection does
+ * indeed contain one and only one element, or throw <code>TestFailedException</code> if not.
  *
- * You should only use this trait when you are not using ShouldMatchers, if you use ShouldMatchers, there's no need to use this trait anymore as 
- * ShouldMatchers already support .loneElement syntax.
+ * <p>
+ * This construct allows you to express in one statement that a collection should contain one and only one element
+ * and that the element value should meet some expectation. Here's an example:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * set.loneElement should be &gt; 9
+ * </pre>
+ *
+ * <p>
+ * Or, using an assertion instead of a matcher expression:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * assert(set.loneElement &gt; 9)
+ * </pre>
  */
 trait LoneElement {
 
-  final class LoneElementTraversableWrapper[T](xs: GenTraversable[T]) {
-      /**
-       * This method enables syntax such as the following:
-       *
-       * <pre class="stHighlight">
-       * xs.loneElement should be &gt; 9
-       *    ^
-       * </pre>
-       */
-      def loneElement: T = {
-        if (xs.size == 1)
-          xs.head
-        else
-          throw new exceptions.TestFailedException(
-            Some(FailureMessages(
-                   "notLoneElement",
-                   xs,
-                   xs.size)), 
-            None, 
-            1
-          )
-      }
+  /**
+   * Wrapper class that adds a <code>loneElement</code> method to <code>GenTraversable</code>.
+   *
+   * <p>
+   * Through the implicit conversion provided by trait <code>LoneElement</code>, this class allows you to make statements like:
+   * </p>
+   *
+   * <pre class="stHighlight">
+   * trav.loneElement should be &gt; 9
+   * </pre>
+   *
+   * @param trav A <code>GenTraversable</code> to wrap in a <code>LoneElementTraversableWrapper</code>, which provides the <code>loneElement</code> method.
+   */
+  final class LoneElementTraversableWrapper[T](trav: GenTraversable[T]) {
+
+    /**
+     * Returns the value contained in the wrapped <code>GenTraversable</code>, if it contains one and only one element, else throws <code>TestFailedException</code> with
+     * a detail message describing the problem.
+     *
+     * <p>
+     * This method enables syntax such as the following:
+     * </p>
+     *
+     * <pre class="stHighlight">
+     * trav.loneElement should be &gt; 9
+     *      ^
+     * </pre>
+     */
+    def loneElement: T = {
+      if (trav.size == 1)
+        trav.head
+      else
+        throw new exceptions.TestFailedException(
+          Some(FailureMessages(
+                 "notLoneElement",
+                 trav,
+                 trav.size)), 
+          None, 
+          1
+        )
+    }
   }
 
   /**
-   * This implicit conversion method enables ScalaTest matchers expressions like the following:
+   * Implicit conversion that adds a <code>loneElement</code> method to <code>GenTraversable</code>.
    *
-   * <pre class="stHighlight">
-   * xs.loneElement should be &gt; 9
-   *    ^
-   * </pre>
+   * @param trav the <code>GenTraversable</code> on which to add the <code>loneElement</code> method
    */
-  implicit def convertToTraversableLoneElementWrapper[T](xs: GenTraversable[T]): LoneElementTraversableWrapper[T] = new LoneElementTraversableWrapper[T](xs)
+  implicit def convertToTraversableLoneElementWrapper[T](trav: GenTraversable[T]): LoneElementTraversableWrapper[T] = new LoneElementTraversableWrapper[T](trav)
 }
+
+/**
+ * Companion object that facilitates the importing of <code>LoneElement</code> members as 
+ * an alternative to mixing it in. One use case is to import <code>LoneElement</code>'s members so you can use
+ * <code>loneElement</code> in the Scala interpreter.
+ */
+object LoneElement extends LoneElement
 
