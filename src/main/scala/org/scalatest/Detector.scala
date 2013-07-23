a7fb58c1c3054726fae6270887d57f9c7933cfff
bvenners <bill@artima.com>
2013-07-22 22:33:35 -0500

    Bumped version to 2.0.M6-SNAP35! Added comments to Detector, which I'm about to change to Extractor/Catcher.

--- src/main/scala/org/scalatest/Detector.scala
+++ src/main/scala/org/scalatest/Detector.scala
@@ -15,6 +15,43 @@
  */
 package org.scalatest
 
+/*
+ Describe need to deal with flickers. Fix them so they pass 100% if you can, but if not, the problem
+ is wastes people's time looking at them. Or if they happen too often it is the cry wolf problem, and people
+ don't bother looking anymore when the CI server fails. They get jaded and red doesn't mean the same.
+ Best thing to do is fix it, but sometimes that's not practical. In such situations, cancel when the 
+ problem occurs.
+
+ If just needed one place, just write it in place
+
+try ...
+catch {
+  case e: DBAccessException if e.getMessage == "500:Internal Server Error" => cancel(e)
+}
+
+ But if you're going to use it in multiple places, make an extractor. This is what it would look like written out in full:
+
+object InternalServerError {
+  def unapply(e: Throwable): Option[Throwable] = {
+    case e: DBAccessException if e.getMessage == "500:Internal Server Error" => Some(e)
+    case _ => None
+  }
+}
+
+  Extractor gives you a factory method for such one-item extractors:
+
+val InternalServerError =
+  Extractor[Throwable] { e: DBAccessException =>
+    e.getMessage == "500:Internal Server Error"
+  }
+
+// Catcher is a convenience subclass for Extractor[Throwable]:
+
+val InternalServerError =
+  Catcher { e: DBAccessException =>
+    e.getMessage == "500:Internal Server Error"
+  }
+*/
 final case class Detector(partial: PartialFunction[Throwable, Boolean]) {
   if (partial == null) throw new NullPointerException("partial was null")
 
