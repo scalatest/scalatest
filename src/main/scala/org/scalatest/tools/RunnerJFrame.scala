84c55ed7c2356a765898ae7c37638eddae44d896
cheeseng <cheeseng@amaseng.com>
2013-08-01 13:29:02 +0800

    Added the mising Test Canceled Reports to View menu in GUI, and make Rerun to work with canceled tests.

--- src/main/scala/org/scalatest/tools/RunnerJFrame.scala
+++ src/main/scala/org/scalatest/tools/RunnerJFrame.scala
@@ -717,6 +717,7 @@ private[scalatest] class RunnerJFrame(
     viewMenu.add(optionsMap(PresentTestFailed))
     viewMenu.add(optionsMap(PresentTestIgnored))
     viewMenu.add(optionsMap(PresentTestPending))
+    viewMenu.add(optionsMap(PresentTestCanceled))
     viewMenu.add(optionsMap(PresentScopeOpened))
     viewMenu.add(optionsMap(PresentScopeClosed))
     viewMenu.add(optionsMap(PresentScopePending))
@@ -861,6 +862,7 @@ private[scalatest] class RunnerJFrame(
         case e: TestStarting => e.rerunner
         case e: TestSucceeded => e.rerunner
         case e: TestFailed => e.rerunner
+        case e: TestCanceled => e.rerunner
         case e: SuiteStarting => e.rerunner
         case e: SuiteCompleted => e.rerunner
         case e: SuiteAborted => e.rerunner
@@ -1342,6 +1344,7 @@ private[scalatest] class RunnerJFrame(
             case e: TestStarting => Some(new TestRerunner(rerunner, e.testName))
             case e: TestSucceeded => Some(new TestRerunner(rerunner, e.testName))
             case e: TestFailed => Some(new TestRerunner(rerunner, e.testName))
+            case e: TestCanceled => Some(new TestRerunner(rerunner, e.testName))
             case e: SuiteStarting => Some(new SuiteRerunner(rerunner))
             case e: SuiteCompleted => Some(new SuiteRerunner(rerunner))
             case e: SuiteAborted => Some(new SuiteRerunner(rerunner))
