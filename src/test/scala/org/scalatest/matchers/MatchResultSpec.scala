b01e636a85fa143ef8d64e54f5d7f121e3b006c4
bvenners <bill@artima.com>
2013-08-06 15:39:27 -0700

    Added tests for and/or.

--- src/test/scala/org/scalatest/matchers/MatchResultSpec.scala
+++ src/test/scala/org/scalatest/matchers/MatchResultSpec.scala
@@ -17,8 +17,9 @@ package org.scalatest.matchers
 
 import org.scalatest._
 import Inside._
+import org.scalautils.PrettyMethods
 
-class MatchResultSpec extends FreeSpec with Matchers {
+class MatchResultSpec extends FreeSpec with Matchers with PrettyMethods {
 
   "A MatchResult" - {
     val mr = MatchResult(false, "1 did not equal 2", "1 equaled 2", "1 did not equal 2", "1 equaled 2")
@@ -177,25 +178,136 @@ class MatchResultSpec extends FreeSpec with Matchers {
   }
 
   "The MatchResult obtained from and-ing two Matchers" - {
+    /*
+      scala> be > 'b' and be > 'd'
+      res0: org.scalatest.matchers.Matcher[Char] = <function1>
+    */
     "should be lazy about constructing strings" - {
-      "for false and false" is pending
-       // scala> be > 'c' and be > 'd'
-       // res4: org.scalatest.matchers.Matcher[Char] = <function1>
+      /*
+        scala> res0('a')
+        res1: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'b','a' was greater than 'b','a' was not greater than 'b','a' was greater than 'b',Vector(),Vector())
+      */
+      "for false and false" in {
+        val m = be > 'b' and be > 'd'
+        val mr = m('a')
+        mr.matches shouldBe false
+        mr.failureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        mr.negatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+        mr.midSentenceFailureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        mr.midSentenceNegatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+      }
 
-       // scala> res4('a')
-       // res5: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'c','a' was greater than 'c','a' was not greater than 'c','a' was greater than 'c',Vector(),Vector())
+       /*
+         scala> be > 'b' and be < 'd'
+         res4: org.scalatest.matchers.Matcher[Char] = <function1>
 
-      "for false and true" is pending
-      "for true and false" is pending
-      "for true and true" is pending
+         scala> res4('a')
+         res5: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'b','a' was greater than 'b','a' was not greater than 'b','a' was greater than 'b',Vector(),Vector())
+       */
+      "for false and true" in {
+        val m = be > 'b' and be < 'd'
+        val mr = m('a')
+        mr.matches shouldBe false
+        mr.failureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        mr.negatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+        mr.midSentenceFailureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        mr.midSentenceNegatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+      }
+
+        /*
+          scala> res0('c')
+          res2: org.scalatest.matchers.MatchResult = MatchResult(false,'c' was greater than 'b', but 'c' was not greater than 'd','c' was greater than 'b', and 'c' was greater than 'd','c' was greater than 'b', but 'c' was not greater than 'd','c' was greater than 'b', and 'c' was greater than 'd',Vector(),Vector())
+        */
+      "for true and false" in {
+        val m = be > 'b' and be > 'd'
+        val mr = m('c')
+        mr.matches shouldBe false
+        mr.failureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'c'.pretty, 'd'.pretty)))
+        mr.negatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'c'.pretty, 'd'.pretty)))
+        mr.midSentenceFailureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'c'.pretty, 'd'.pretty)))
+        mr.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'c'.pretty, 'd'.pretty)))
+      }
+
+        /*
+          scala> res0('e')
+          res3: org.scalatest.matchers.MatchResult = MatchResult(true,'e' was greater than 'b', but 'e' was not greater than 'd','e' was greater than 'b', and 'e' was greater than 'd','e' was greater than 'b', but 'e' was not greater than 'd','e' was greater than 'b', and 'e' was greater than 'd',Vector(),Vector())
+        */
+      "for true and true" in {
+        val m = be > 'b' and be > 'd'
+        val mr = m('e')
+        mr.matches shouldBe true
+        mr.failureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'e'.pretty, 'd'.pretty)))
+        mr.negatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'e'.pretty, 'd'.pretty)))
+        mr.midSentenceFailureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'e'.pretty, 'd'.pretty))) 
+        mr.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'e'.pretty, 'd'.pretty)))
+      }
     }
   }
   "The MatchResult obtained from or-ing two Matchers" - {
+    /*
+      scala> be > 'b' or be > 'd'
+      res0: org.scalatest.matchers.Matcher[Char] = <function1>
+    */
     "should be lazy about constructing strings" - {
-      "for false and false" is pending
-      "for false and true" is pending
-      "for true and false" is pending
-      "for true and true" is pending
+
+      /*
+      scala> res0('a')
+      res1: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'b', and 'a' was not greater than 'd','a' was not greater than 'b', and 'a' was greater than 'd','a' was not greater than 'b', and 'a' was not greater than 'd','a' was not greater than 'b', and 'a' was greater than 'd',Vector(),Vector())
+      */
+      "for false or false" in {
+        val m = be > 'b' or be > 'd'
+        val mr = m('a')
+        mr.matches shouldBe false
+        mr.failureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'a'.pretty, 'd'.pretty)))
+        mr.negatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'a'.pretty, 'd'.pretty)))
+        mr.midSentenceFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'a'.pretty, 'd'.pretty)))
+        mr.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'a'.pretty, 'd'.pretty)))
+       }
+
+      /*
+        scala> be > 'b' or be < 'd'
+        res4: org.scalatest.matchers.Matcher[Char] = <function1>
+
+        scala> res4('a')
+        res5: org.scalatest.matchers.MatchResult = MatchResult(true,'a' was not greater than 'b', and 'a' was not less than 'd','a' was not greater than 'b', and 'a' was less than 'd','a' was not greater than 'b', and 'a' was not less than 'd','a' was not greater than 'b', and 'a' was less than 'd',Vector(),Vector())
+      */
+      "for false or true" in {
+        val m = be > 'b' or be < 'd'
+        val mr = m('a')
+        mr.matches shouldBe true
+        mr.failureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotLessThan", 'a'.pretty, 'd'.pretty)))
+        mr.negatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasLessThan", 'a'.pretty, 'd'.pretty)))
+        mr.midSentenceFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotLessThan", 'a'.pretty, 'd'.pretty)))
+        mr.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasLessThan", 'a'.pretty, 'd'.pretty)))
+      }
+
+      /*
+      scala> res0('c')
+      res2: org.scalatest.matchers.MatchResult = MatchResult(true,'c' was greater than 'b','c' was not greater than 'b','c' was greater than 'b','c' was not greater than 'b',Vector(),Vector())
+      */
+      "for true or false" in {
+        val m = be > 'b' or be > 'd'
+        val mr = m('c')
+        mr.matches shouldBe true
+        mr.failureMessage should be (Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty))
+        mr.negatedFailureMessage should be (Resources("wasNotGreaterThan", 'c'.pretty, 'b'.pretty))
+        mr.midSentenceFailureMessage should be (Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty))
+        mr.midSentenceNegatedFailureMessage should be (Resources("wasNotGreaterThan", 'c'.pretty, 'b'.pretty))
+      }
+
+      /*
+      scala> res0('e')
+      res3: org.scalatest.matchers.MatchResult = MatchResult(true,'e' was greater than 'b','e' was not greater than 'b','e' was greater than 'b','e' was not greater than 'b',Vector(),Vector())
+      */
+      "for true or true" in {
+        val m = be > 'b' or be > 'd'
+        val mr = m('e')
+        mr.matches shouldBe true
+        mr.failureMessage should be (Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty))
+        mr.negatedFailureMessage should be (Resources("wasNotGreaterThan", 'e'.pretty, 'b'.pretty))
+        mr.midSentenceFailureMessage should be (Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty))
+        mr.midSentenceNegatedFailureMessage should be (Resources("wasNotGreaterThan", 'e'.pretty, 'b'.pretty))
+      }
     }
   }
 }
