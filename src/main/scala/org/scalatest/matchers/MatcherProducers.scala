7fe45524f3a134faa424446f7db6c8198d8896aa
cheeseng <cheeseng@amaseng.com>
2013-09-04 12:58:14 +0800

    Added scaladoc to MatcherProducers.

--- src/main/scala/org/scalatest/matchers/MatcherProducers.scala
+++ src/main/scala/org/scalatest/matchers/MatcherProducers.scala
@@ -15,7 +15,53 @@
  */
 package org.scalatest.matchers
 
+/**
+ * Trait that provides the ability to modify error messages when composing <code>Matcher</code>s.
+ *
+ * @author Bill Venners
+ * @author Chee Seng
+ */
 trait MatcherProducers {
+  /**
+   * Implicit conversion that converts a function of <code>T => Matcher[T]</code> to an object that has
+   * <code>composeTwice</code>, <code>mapResult</code> and <code>mapArgs</code> methods.
+   *
+   * The following shows how this trait is used to compose twice and modify error messages:
+   *
+   * <pre class="stHighlight">
+   * import org.scalatest._
+   * import matchers._
+   * import MatcherProducers._
+   *
+   * val f = be > (_: Int)
+   * val g = (_: String).toInt
+   *
+   * // f composeTwice g means: (f compose g) andThen (_ compose g)
+   * val beAsIntsGreaterThan = f composeTwice g mapResult { mr =>
+   *   mr.copy(
+   *     failureMessageArgs = mr.failureMessageArgs.map((LazyArg(_) { _.toString + ".toInt"})),
+   *     negatedFailureMessageArgs = mr.negatedFailureMessageArgs.map((LazyArg(_) { _.toString + ".toInt"})),
+   *     midSentenceFailureMessageArgs =
+   *       mr.midSentenceFailureMessageArgs.map((LazyArg(_) { _.toString + ".toInt"})),
+   *     midSentenceNegatedFailureMessageArgs =
+   *       mr.midSentenceNegatedFailureMessageArgs.map((LazyArg(_) { _.toString + ".toInt"}))
+   *   )
+   * }
+   *
+   * "7" should beAsIntsGreaterThan ("8")
+   *
+   * </pre>
+   *
+   * The last assertion will fail with message like this:
+   *
+   * <pre class="stHighlight">
+   * 7.toInt was not greater than 8.toInt
+   * </pre>
+   *
+   * @param f a function that takes a <code>T</code> and return a <code>Matcher[T]</code>
+   * @tparam T the type used by function <code>f</code>
+   * @return an object that has <code>composeTwice</code>, <code>mapResult</code> and <code>mapArgs</code> methods.
+   */
   implicit def decorate[T](f: T => Matcher[T]) = 
     new {
       def composeTwice[U](g: U => T): U => Matcher[U] = (f compose g) andThen (_ compose g)
@@ -26,5 +72,10 @@ trait MatcherProducers {
     }
 }
 
+/**
+ * Companion object that facilitates the importing of <code>MatcherProducers</code> members as
+ * an alternative to mixing it in. One use case is to import <code>MatcherProducers</code>'s members so you can use
+ * <code>MatcherProducers</code> in the Scala interpreter.
+ */
 object MatcherProducers extends MatcherProducers
 
