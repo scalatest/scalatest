10e76eb0250ebc55043ec15dca5b327e8debf8db
bvenners <bill@artima.com>
2013-09-20 20:52:57 -0700

    Fleshed out Scaladoc for Every.

--- src/main/scala/org/scalautils/Every.scala
+++ src/main/scala/org/scalautils/Every.scala
@@ -27,16 +27,250 @@ import Every.fromNonEmptyVector
 import scala.annotation.unchecked.{ uncheckedVariance => uV }
 
 // Can't be an IndexedSeq[T] because Builder would be able to create an empty one.
+/**
+ * An ordered, immutable, non-empty collection of elements.
+ *
+ * <p>
+ * Class <code>Every</code> has two and only two subtypes: <a href="One.html"><code>One</code></a> and <a href="Many.html"><code>Many</code></a>.
+ * A <code>One</code> contains exactly one element. A <code>Many</code> contains two or more elements. Thus no way exists for an
+ * <code>Every</code> to contain zero elements.
+ * </p>
+ *
+ * <h2>Constructing <code>Every</code>s</h2>
+ *
+ * <p>
+ * You can construct an <code>Every</code> by passing one or more elements to the <code>Every.apply</code> factory method:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * Every(1)
+ * Every(1, 2)
+ * Every(1, 2, 3)
+ * </pre>
+ *
+ * <p>
+ * Alternatively you can pass one element to the <code>One.apply</code> factory method, or two or more elements to 
+ * <code>Many.apply</code>:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * One(1)
+ * Many(1, 3)
+ * Many(1, 2, 3)
+ * </pre>
+ *
+ * <h2>Working with <code>Every</code>s</h2>
+ *
+ * <p>
+ * <code>Every</code> does not extend Scala's <code>Seq</code> or <code>Traversable</code> traits because these require that
+ * implementations may be empty. For example, if you invoke <code>tail</code> on a <code>Seq</code> that contains just one element,
+ * you'll get an empty <code>Seq</code>:
+ * </p>
+ * 
+ * <pre class="stREPL">
+ * scala&gt; List(1).tail
+ * res6: List[Int] = List()
+ * </pre>
+ *
+ * <p>
+ * On the other hand, many useful methods exist on <code>Seq</code> that when invoked on a non-empty <code>Seq</code> are guaranteed
+ * to not result in an empty <code>Seq</code>. For convenience, <code>Every</code> defines a method corresponding to every such <code>Seq</code>
+ * method. Here are some examples:
+ * </p>
+ * 
+ * <pre class="stHighlight">
+ * Many(1, 2, 3).map(_ + 1) // Result: Many(2, 3, 4)
+ * One(1).map(_ + 1)        // Result: One(2)
+ * </pre>
+ *
+ * <p>
+ * <code>Every</code> does <em>not</em> currently define any methods corresponding to <code>Seq</code> methods that could result in
+ * an empty <code>Seq</code>. However, an implicit converison from <code>Every</code> to <code>collection.immutable.IndexedSeq</code>
+ * is defined in the <code>Every</code> companion object that will be applied if you attempt to call one of the missing methods. As a
+ * result, you can invoke <code>filter</code> on an <code>Every</code>, even though <code>filter</code> could result
+ * in an empty sequence&mdash;but the result type will be <code>collection.immutable.IndexedSeq</code> instead of <code>Every</code>:
+ * </p>
+ *
+ * <pre class="stHighlight">
+ * Every(1, 2, 3).filter(_ &lt; 10) // Result: Vector(1, 2, 3)
+ * Every(1, 2, 3).filter(_ &gt; 10) // Result: Vector()
+ * </pre>
+ * 
+ *
+ * <p>
+ * You can use <code>Every</code>s in <code>for</code> expressions. The result will be an <code>Every</code> unless
+ * you use a filter (an <code>if</code> clause). Because filters are desugared to invocations of <code>filter</code>, the
+ * result type will switch to a <code>collection.immutable.IndexedSeq</code> at that point. Here are some examples:
+ * </p>
+ * 
+ * <pre class="stREPL">
+ * scala&gt; import org.scalautils._
+ * import org.scalautils._
+ *
+ * scala&gt; for (i &lt;- Every(1, 2, 3)) yield i + 1
+ * res0: org.scalautils.Every[Int] = Many(2, 3, 4)
+ *
+ * scala&gt; for (i &lt;- Every(1, 2, 3) if i &lt; 10) yield i + 1
+ * res1: scala.collection.immutable.IndexedSeq[Int] = Vector(2, 3, 4)
+ *
+ * scala&gt; for {
+ *      |   i &lt;- Every(1, 2, 3)
+ *      |   j &lt;- Every('a', 'b', 'c')
+ *      | } yield (i, j)
+ * res3: org.scalautils.Every[(Int, Char)] =
+ *         Many((1,a), (1,b), (1,c), (2,a), (2,b), (2,c), (3,a), (3,b), (3,c))
+ *
+ * scala&gt; for {
+ *      |   i &lt;- Every(1, 2, 3) if i &lt; 10
+ *      |   j &lt;- Every('a', 'b', 'c')
+ *      | } yield (i, j)
+ * res6: scala.collection.immutable.IndexedSeq[(Int, Char)] =
+ *         Vector((1,a), (1,b), (1,c), (2,a), (2,b), (2,c), (3,a), (3,b), (3,c))
+ * </pre>
+ *
+ * <h2>Motivation for <code>Every</code>s</h2>
+ *
+ * <p>
+ * Although <code>Every</code> is a general-purpose, non-empty ordered collection, it was motivated by the desire to enable easy
+ * accumulation of errors in <a href="Or.html"><code>Or</code></a>s. For examples of <code>Every</code> used in that use case, see the
+ * <a href="Or.html#accumulatingErrors">Accumulating errors with <code>Or</code></a> section in the main documentation for <code>Or</code>.
+ * </p>
+ *
+ * @tparam T the type of elements contained in this <code>Every</code>
+ */
 sealed abstract class Every[+T] protected (underlying: Vector[T]) extends PartialFunction[Int, T] {
+
+  /**
+   * Returns a new <code>Many</code> containing the elements of this <code>Every</code> followed by the elements of the passed <code>Every</code>.
+   * The element type of the resulting <code>Many</code> is the most specific superclass encompassing the element types of this and the passed <code>Every</code>.
+   *
+   * @tparam U the element type of the returned <code>Many</code>
+   * @param other the <code>Every</code> to append
+   * @return a new <code>Many</code> that contains all the elements of this <code>Every</code> followed by all elements of <code>other</code>.
+   */
   def ++[U >: T](other: Every[U]): Many[U]
+
+  /**
+   * Returns a new <code>Many</code> containing the elements of this <code>Every</code> followed by the elements of the passed <code>GenTraversableOnce</code>.
+   * The element type of the resulting <code>Many</code> is the most specific superclass encompassing the element types of this <code>Every</code>
+   * and the passed <code>GenTraversableOnce</code>.
+   *
+   * @tparam U the element type of the returned <code>Many</code>
+   * @param other the <code>Every</code> to append
+   * @return a new <code>Many</code> that contains all the elements of this <code>Every</code> followed by all elements of <code>other</code>.
+   */
   def ++[U >: T](other: GenTraversableOnce[U]): Every[U]
+
+  /**
+   * Fold left: applies a binary operator to a start value, <code>z</code>, and all elements of this <code>Every</code>, going left to right.
+   *
+   * <p>
+   * Note: <code>/:</code> is alternate syntax for the <code>foldLeft</code> method; <code>z</code> <code>/:</code> <code>every</code> is the
+   * same as <code>every</code> <code>foldLeft</code> <code>z</code>.
+   * </p>
+   *
+   * @tparam B the result of the binary operator
+   * @param z the start value
+   * @param op the binary operator
+   * @return the result of inserting <code>op</code> between consecutive elements of this <code>Every</code>, going left to right, with the start value,
+   *     <code>z</code>, on the left:
+   *
+   * <pre>
+   * op(...op(op(z, x_1), x_2), ..., x_n)
+   * </pre>
+   *
+   * <p>
+   * where x<sub>1</sub>, ..., x<sub>n</sub> are the elements of this <code>Every</code>. 
+   * </p>
+   */
   final def /:[B](z: B)(op: (B, T) => B): B = underlying./:(z)(op)
+
+  /**
+   * Fold right: applies a binary operator to all elements of this <code>Every</code> and a start value, going right to left.
+   *
+   * <p>
+   * Note: <code>:\</code> is alternate syntax for the <code>foldRight</code> method; <code>every</code> <code>:\</code> <code>z</code> is the same
+   * as <code>every</code> <code>foldRight</code> <code>z</code>.
+   * </p>
+   *
+   * @tparam B the result of the binary operator
+   * @param z the start value
+   * @param op the binary operator
+   * @return the result of inserting <code>op</code> between consecutive elements of this <code>Every</code>, going right to left, with the start value,
+   *     <code>z</code>, on the right:
+   *
+   * <pre>
+   * op(x_1, op(x_2, ... op(x_n, z)...))
+   * </pre>
+   *
+   * <p>
+   * where x<sub>1</sub>, ..., x<sub>n</sub> are the elements of this <code>Every</code>. 
+   * </p>
+   */
   final def :\[B](z: B)(op: (T, B) => B): B = underlying.:\(z)(op)
+
+  /**
+   * Returns a new <code>Every</code> with the given element prepended.
+   *
+   * <p>
+   * Note that :-ending operators are right associative. A mnemonic for <code>+:</code> <em>vs.</em> <code>:+</code> is: the COLon goes on the COLlection side.
+   * </p>
+   *
+   * @param element the element to prepend to this <code>Every</code>
+   * @return a new <code>Every</code> consisting of <code>element</code> followed by all elements of this <code>Every</code>.
+   */
   final def +:[U >: T](element: U): Many[U] = Many(element, underlying.head, underlying.tail: _*)
+
+  /**
+   * Returns a new <code>Every</code> with the given element appended.
+   *
+   * <p>
+   * Note a mnemonic for <code>+:</code> <em>vs.</em> <code>:+</code> is: the COLon goes on the COLlection side.
+   * </p>
+   *
+   * @param element the element to append to this <code>Every</code>
+   * @return a new <code>Every</code> consisting of all elements of this <code>Every</code> followed by <code>element</code>.
+   */
   def :+[U >: T](element: U): Many[U]
+
+  /**
+   * Appends all elements of this <code>Every</code> to a string builder. The written text will consist of a concatenation of the result of invoking <code>toString</code>
+   * on of every element of this <code>Every</code>, without any separator string.
+   *
+   * @param sb the string builder to which elements will be appended
+   * @return the string builder, <code>sb</code>, to which elements were appended.
+   */
   final def addString(sb: StringBuilder): StringBuilder = underlying.addString(sb)
+
+  /**
+   * Appends all elements of this <code>Every</code> to a string builder using a separator string. The written text will consist of a concatenation of the
+   * result of invoking <code>toString</code>
+   * on of every element of this <code>Every</code>, separated by the string <code>sep</code>.
+   *
+   * @param sb the string builder to which elements will be appended
+   * @param sep the separator string
+   * @return the string builder, <code>sb</code>, to which elements were appended.
+   */
   final def addString(sb: StringBuilder, sep: String): StringBuilder = underlying.addString(sb, sep)
+
+  /**
+   * Appends all elements of this <code>Every</code> to a string builder using start, end, and separator strings. The written text will consist of a concatenation of
+   * the string <code>start</code>; the result of invoking <code>toString</code> on all elements of this <code>Every</code>,
+   * separated by the string <code>sep</code>; and the string <code>end</code>
+   *
+   * @param sb the string builder to which elements will be appended
+   * @param start the starting string
+   * @param sep the separator string
+   * @param start the ending string
+   * @return the string builder, <code>sb</code>, to which elements were appended.
+   */
   final def addString(sb: StringBuilder, start: String, sep: String, end: String): StringBuilder = underlying.addString(sb, start, sep, end)
+
+  /**
+   * Selects an element by its index in the <code>Every</code>.
+   *
+   * @return the element of this <code>Every</code> at index <code>idx</code>, where 0 indicates the first element.
+   */
   final def apply(idx: Int): T = underlying(idx)
   final def collectFirst[U](pf: PartialFunction[T, U]): Option[U] = underlying.collectFirst(pf)
   final def contains(elem: Any): Boolean = underlying.contains(elem)
@@ -220,6 +454,16 @@ object Every {
   private def fromNonEmptyVector[E](vec: Vector[E]): Every[E] = Every(vec.head, vec.tail: _*)
 }
 
+/**
+ * An <code>Every</code> that contains exactly one element.
+ *
+ * <p>
+ * For more information and examples, see the main documentation for superclass <a href="Every.html"><code>Every</code></a>.
+ * </p>
+ *
+ * @tparam T the type of the element contained in this <code>One</code>
+ * @param loneElement the lone element contained in this <code>One</code>
+ */
 final case class One[+T](loneElement: T) extends Every[T](Vector(loneElement)) {
   def asEvery: Every[T] = this
   def ++[U >: T](other: Every[U]): Many[U] = Many(loneElement, other.toVector.head, other.toVector.tail: _*)
@@ -230,6 +474,18 @@ final case class One[+T](loneElement: T) extends Every[T](Vector(loneElement)) {
   override def toString: String = "One(" + loneElement + ")"
 }
 
+/**
+ * An <code>Every</code> that contains two or more elements.
+ *
+ * <p>
+ * For more information and examples, see the main documentation for superclass <a href="Every.html"><code>Every</code></a>.
+ * </p>
+ *
+ * @tparam T the type of the element contained in this <code>Many</code>
+ * @param firstElement the first element (with index 0) contained in this <code>Many</code>
+ * @param secondElement the second element (with index 1) contained in this <code>Many</code>
+ * @param otherElement a varargs of zero or more other elements (with index 2, 3, ...) contained in this <code>Many</code>
+ */
 final case class Many[+T](firstElement: T, secondElement: T, otherElements: T*) extends Every[T](firstElement +: secondElement +: Vector(otherElements: _*)) {
   def asEvery: Every[T] = this
   def ++[U >: T](other: Every[U]): Many[U] = Many(firstElement, secondElement, (otherElements.toVector ++ other.toVector): _*)
@@ -239,6 +495,5 @@ final case class Many[+T](firstElement: T, secondElement: T, otherElements: T*) 
   def stringPrefix: String = "Many"
   override def toString: String = "Many(" + toVector.mkString(", ") + ")"
 }
-
 
 
