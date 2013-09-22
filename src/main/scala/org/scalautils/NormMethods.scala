fd586c047b224df13b299e11c3c86ff3932cc350
bvenners <bill@artima.com>
2013-09-22 14:45:06 -0700

    Dropped incorrect Scaladoc comments in NormMethods.

--- src/main/scala/org/scalautils/NormMethods.scala
+++ src/main/scala/org/scalautils/NormMethods.scala
@@ -15,89 +15,12 @@
  */
 package org.scalautils
 
-/**
- * Defines a custom way to determine equality for a type.
- *
- * <p>
- * For example, here's how you could define equality between <code>Double</code>s such that
- * a tolerance of &plusmn; 0.01 is allowed:
- * </p>
- *
- * <pre class="stHighlight">
- * class TolerantDoubleEquality extends Equality[Double] {
- *
- *   private val Tol = 0.01
- *
- *   def areEqual(a: Double, b: Any): Boolean = {
- *     b match {
- *       case bDouble: Double =&gt; (a <= bDouble + Tol) && (a >= bDouble - Tol)
- *       case _ =&gt; false
- *     }
- *   }
- * }
- * </pre>
- *
- * <p>
- * If an implicit instance of <code>TolerantDoubleEquality</code> is in scope, it will be
- * used by ScalaTest's <code>===</code> operators and its <code>should equal</code> and <code>should ===</code> matcher
- * syntax. Here's an example:
- * </p>
- *
- * <pre class="stREPL">
- * $ scala -cp target/jar_contents/
- * Welcome to Scala version 2.10.0 (Java HotSpot(TM) 64-Bit Server VM, Java 1.6.0_33).
- * Type in expressions to have them evaluated.
- * Type :help for more information.
-
- * scala&gt; import org.scalautils._
- * import org.scalautils._
-
- * scala&gt; class TolerantDoubleEquality extends Equality[Double] {
- *      | 
- *      |   private val Tol = 0.01
- *      | 
- *      |   def areEqual(a: Double, b: Any): Boolean = {
- *      |     b match {
- *      |       case bDouble: Double =&gt; (a &gt;= bDouble + Tol) && (a &gt;= bDouble - Tol)
- *      |       case _ =&gt; false
- *      |     }
- *      |   }
- *      | }
- * defined class TolerantDoubleEquality
- *
- * scala&gt; import TripleEquals._
- * import TripleEquals._
- *
- * scala&gt; 2.0 === 2.001
- * res0: Boolean = false
- *
- * scala&gt; implicit val tolerantDoubleEquality = new TolerantDoubleEquality
- * tolerantDoubleEquality: TolerantDoubleEquality = TolerantDoubleEquality@70c13c17
- *
- * scala&gt; 2.0 === 2.001
- * res1: Boolean = true
- * </pre>
- *
- * <p>
- * <em>Note: The <code>Equality</code> type class was inspired in part by the <code>Equal</code> type class of the 
- * <a href="http://code.google.com/p/scalaz/" target="_blank"><code>scalaz</code></a> project.</em>
- * </p>
- *
- * @tparam A the type whose equality is being customized
- */
 trait NormMethods {
 
   final class Normalizer[T](o: T)(implicit normalization: Normalization[T]) {
     def norm: T = normalization.normalized(o)
   }
 
-  /**
-   * Indicates whether the objects passed as <code>a</code> and <code>b</code> are equal.
-   *
-   * @param a a left-hand-side object being compared with another (right-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
-   * @param b a right-hand-side object being compared with another (left-hand-side one) for equality (<em>e.g.</em>, <code>a == b</code>)
-   * @return true if the passed objects are "equal," as defined by this <code>Equality</code> instance
-   */
   implicit def convertToNormalizer[T](o: T)(implicit normalization: Normalization[T]): Normalizer[T] = new Normalizer[T](o)
 } 
 
