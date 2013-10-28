e462890a412ce735145a11c13133be0e083577e3
cheeseng <cheeseng@amaseng.com>
2013-10-28 11:20:21 +0800

    Fixed case problem in path to flatSpec.gif.

--- src/main/scala/org/scalatest/FlatSpec.scala
+++ src/main/scala/org/scalatest/FlatSpec.scala
@@ -440,7 +440,7 @@ import Suite.autoTagClassAnnotations
  * add nicely formatted text to HTML reports. Here's what the above <code>SetSpec</code> would look like in the HTML reporter:
  * </p>
  *
- * <img class="stScreenShot" src="../../lib/flatspec.gif">
+ * <img class="stScreenShot" src="../../lib/flatSpec.gif">
  *
  * <a name="notifiersAlerters"></a><h2>Notifiers and alerters</h2></a>
  *
