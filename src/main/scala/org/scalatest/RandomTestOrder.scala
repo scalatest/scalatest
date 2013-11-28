6b03b2d178d44380f0855c5db554ea475e8bc940
cheeseng <cheeseng@amaseng.com>
2013-11-28 21:54:29 +0800

    Work in progress RandomTestOrder, got initial version of running tests in initial order, but not yet with ordered events.

---
+++ src/main/scala/org/scalatest/RandomTestOrder.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+import scala.util.Random
+import collection.mutable.ListBuffer
+
+trait RandomTestOrder extends SuiteMixin {
+
+  this: Suite =>
+
+  case class DeferredSuiteRun(suite: Suite with RandomTestOrder, testName: String, status: ScalaTestStatefulStatus)
+
+  private val suiteList = new ListBuffer[DeferredSuiteRun]
+
+  protected abstract override def runTest(testName: String, args: Args): Status = {
+
+    if (args.runTestInNewInstance) {
+      // In initial instance, so create a new test-specific instance for this test and invoke run on it.
+      val oneInstance = newInstance
+      // defer the suite execution
+      val status = new ScalaTestStatefulStatus
+      suiteList += DeferredSuiteRun(oneInstance, testName, status)
+      status
+    }
+    else {// Therefore, in test-specific instance, so run the test.
+      super.runTest(testName, args)
+    }
+  }
+
+  protected abstract override def runTests(testName: Option[String], args: Args): Status = {
+
+    if (args.runTestInNewInstance) {
+      if (testName.isEmpty)
+        throw new IllegalArgumentException("args.runTestInNewInstance was true, but testName was not defined")
+      // In test-specific instance, so run the test. (We are removing RTINI
+      // so that runTest will realize it is in the test-specific instance.)
+      runTest(testName.get, args.copy(runTestInNewInstance = false))
+    }
+    else {
+      super.runTests(testName, args.copy(runTestInNewInstance = true))
+      // Random shuffle the deferred suite list, before executing them.
+      val statusList: List[Status] =
+        Random.shuffle(suiteList.toList).map { case DeferredSuiteRun(suite, testName, statefulStatus) =>
+          val status = suite.run(Some(testName), args.copy(runTestInNewInstance = true))
+          status.whenCompleted { result =>
+            if (!result)
+              statefulStatus.setFailed()
+            statefulStatus.setCompleted()
+          }
+          statefulStatus
+        }
+      new CompositeStatus(statusList.toSet)
+    }
+  }
+
+  def newInstance: Suite with RandomTestOrder = this.getClass.newInstance.asInstanceOf[Suite with RandomTestOrder]
+}
