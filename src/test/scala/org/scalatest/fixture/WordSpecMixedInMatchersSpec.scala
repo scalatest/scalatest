e1c21dffb6410450d52427c0be2a9e45c36fc7bf
bvenners <bill@artima.com>
2014-01-31 18:05:52 +0800

    inti ver. more test classes that ensure matchers can be either mixed into or imported above FlatSpec and WordSpec, which broke in 2.1.RC1.

---
+++ src/test/scala/org/scalatest/fixture/WordSpecMixedInMatchersSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.fixture
+
+import org.scalatest.{ Matchers, StringFixture }
+
+class WordSpecMixedInMatchersSpec extends WordSpec with Matchers with StringFixture {
+  "This spec" should {
+    "work OK" in { _ =>
+      "hello" should startWith ("he")
+      "hello" should endWith ("lo")
+      "hello" should include ("el")
+      "hello" should startWith regex ("h*")
+      "hello" should endWith regex (".*o")
+      "hello" should include regex ("l*")
+    }
+    "still work OK" in { _ => 
+      "dude" should not startWith ("he")
+      "dude" should not endWith ("lo")
+      "dude" should not include ("el")
+      "dude" should not startWith regex ("h*l")
+      "dude" should not endWith regex ("e*o")
+      "dude" should not include regex ("e*l")
+    }
+  }
+}
+
