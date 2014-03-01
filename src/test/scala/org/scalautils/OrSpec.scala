424e4272ca515c201b351ba86f1570b040db9646
bvenners <bill@artima.com>
2014-03-01 12:03:05 -0600

    Added recover and recoverWith methods to Or.

--- src/test/scala/org/scalautils/OrSpec.scala
+++ src/test/scala/org/scalautils/OrSpec.scala
@@ -112,6 +112,22 @@ class OrSpec extends UnitSpec with Accumulation with TypeCheckedTripleEquals {
     Good(8).orBad[ErrorMessage] badMap (_.toUpperCase) should equal (Good(8))
     Good[Int].orBad("eight") badMap (_.toUpperCase) should equal (Bad("EIGHT"))
   }
+  it can "be used with recover" in {
+    Good(8).orBad[Throwable] recover {
+      case iae: IllegalArgumentException => 9
+    } should equal (Good(8))
+    Good[Int].orBad(new IllegalArgumentException) recover {
+      case iae: IllegalArgumentException => 9
+    } should equal (Good(9))
+  }
+  it can "be used with recoverWith" in {
+    Good(8).orBad[Throwable] recoverWith {
+      case iae: IllegalArgumentException => Good(9)
+    } should equal (Good(8))
+    Good[Int].orBad(new IllegalArgumentException) recoverWith {
+      case iae: IllegalArgumentException => Good(9)
+    } should equal (Good(9))
+  }
   it can "be used with foreach" in {
     var vCount = 0
     var eCount = 0
