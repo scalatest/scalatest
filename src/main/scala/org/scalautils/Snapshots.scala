343cee0a8a343abd87ea7ebacf682d038adfa9ec
cheeseng <cheeseng@amaseng.com>
2014-03-05 14:37:41 +0800

    Added Snapshots.

---
+++ src/main/scala/org/scalautils/Snapshots.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2001-2014 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalautils
+
+import reflect.macros.Context
+
+/**
+ * Case class to store the name and value of an expression.
+ *
+ * @param name the name (as in source) of the expression
+ * @param value the value of the expression
+ */
+case class Snapshot(name: String, value: Any) {
+
+  /**
+   * Overriden <code>toString</code> to print in {name} = {value} format.
+   *
+   * @return string in {name} = {value} format
+   */
+  override def toString: String = Resources("aEqualB", name, Prettifier.default(value))
+
+}
+
+/**
+ * Trait that contains a <code>snap</code> method that can be used to capture snapshot of expression.
+ * A <code>Snapshot</code> contains the name (as in source) and the value of the expression.
+ */
+trait Snapshots {
+
+  import language.experimental.macros
+
+  /**
+   * Snap the given expressions.
+   *
+   * @param expressions expressions to be snapped
+   * @return an <code>IndexedSeq</code> of <code>Snapshot</code> for the given expressions.
+   */
+  def snap(expressions: Any*): IndexedSeq[Snapshot] = macro SnapshotsMacro.snap
+}
+
+/**
+ * Companion object that facilitates the importing of <code>Assertions</code> members as
+ * an alternative to mixing it in. One use case is to import <code>Assertions</code> members so you can use
+ * them in the Scala interpreter:
+ *
+ * <pre class="stREPL">
+ * $scala -classpath scalatest.jar
+ * Welcome to Scala version 2.10.3.final (Java HotSpot(TM) Client VM, Java xxxxxx).
+ * Type in expressions to have them evaluated.
+ * Type :help for more information.
+ * &nbsp;
+ * scala&gt; import org.scalautils.Snapshots._
+ * import org.scalatest.Snapshots._
+ * &nbsp;
+ * scala&gt; val a = 8
+ * a: Int = 8
+ *&nbsp;
+ * scala&gt; snap(a)
+ * res0: scala.collection.immutable.Vector[org.scalautils.Snapshot] = Vector(a = 8)
+ * </pre>
+ */
+object Snapshots extends Snapshots
+
+private[scalautils] object SnapshotsMacro {
+
+  def snap(context: Context)(expressions: context.Expr[Any]*): context.Expr[IndexedSeq[Snapshot]] = {
+    import context.universe._
+
+    val snapshots =
+      expressions.map { expr =>
+        Apply(
+          Select(
+            Select(
+              Select(
+                Ident(newTermName("org")),
+                newTermName("scalautils")
+              ),
+              newTermName("Snapshot")
+            ),
+            newTermName("apply")
+          ),
+          List(context.literal(show(expr.tree)).tree, expr.tree.duplicate)
+        )
+      }
+
+    context.Expr(
+      Apply(
+        Select(
+          Ident("Vector"),
+          newTermName("apply")
+        ),
+        List(snapshots: _*)
+      )
+    )
+  }
+}
---
+++ src/test/scala/org/scalautils/SnapshotsSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2001-2013 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalautils
+
+import org.scalatest._
+
+class SnapshotsSpec extends Spec {
+
+  object `Snapshot ` {
+
+    def `should have a nice toString` {
+      val snapshot = Snapshot("aName", "ScalaTest")
+      assert(snapshot.toString == "aName = \"ScalaTest\"")
+    }
+
+  }
+
+  object `Snapshots should ` {
+
+    def `snap variable names and values and put them in IndexedSeq of Snapshot` {
+
+      val a = 3
+      val b = 4
+      val snapshots = Snapshots.snap(a, b, a + b)
+      assert(snapshots.size == 3)
+      assert(snapshots(0) == Snapshot("a", 3))
+      assert(snapshots(1) == Snapshot("b", 4))
+      assert(snapshots(2) == Snapshot("a.+(b)", 7))
+    }
+
+    def `snap variable names and values and user can transform them using map` {
+      val a = 3
+      val b = 4
+      val snapshots = Snapshots.snap(a, b)
+      val result = snapshots.map(v => v.name + " was equal, not surprisingly, to " + v.value).mkString("\n")
+      assert(result == "a was equal, not surprisingly, to 3\n" +
+                       "b was equal, not surprisingly, to 4")
+    }
+
+  }
+
+}
+
