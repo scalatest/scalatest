ea924dc5b18adba47549c23791a93a3b6037c074
cheeseng <cheeseng@amaseng.com>
2014-04-11 20:43:33 +0800

    Added more tests of registerTest and registerIgnoredTests for WordSpec and fixture.WordSpec.

--- src/test/scala/org/scalatest/WordSpecSpec.scala
+++ src/test/scala/org/scalatest/WordSpecSpec.scala
@@ -246,6 +246,19 @@ class WordSpecSpec extends FunSpec with GivenWhenThen {
         val spec = new MySpec
         ensureTestFailedEventReceived(spec, "should blow up")
       }
+      it("should, if they call a nested registerTest with tags from within a registerTest clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          registerTest("should blow up") {
+            registerTest("should never run", mytags.SlowAsMolasses) {
+              assert(1 == 1)
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
       it("should, if they call a describe with a nested ignore from within an it clause, result in a TestFailedException when running the test") {
 
         class MySpec extends WordSpec {
@@ -280,6 +293,19 @@ class WordSpecSpec extends FunSpec with GivenWhenThen {
           "should blow up" in {
             "should never run" taggedAs(mytags.SlowAsMolasses) ignore {
               assert(1 === 1)
+            }
+          }
+        }
+
+        val spec = new MySpec
+        ensureTestFailedEventReceived(spec, "should blow up")
+      }
+      it("should, if they call a nested registerIgnoredTest with tags from within a registerTest clause, result in a TestFailedException when running the test") {
+
+        class MySpec extends WordSpec {
+          registerTest("should blow up") {
+            registerIgnoredTest("should never run", mytags.SlowAsMolasses) {
+              assert(1 == 1)
             }
           }
         }
@@ -452,6 +478,7 @@ class WordSpecSpec extends FunSpec with GivenWhenThen {
           "hi" taggedAs(mytags.SlowAsMolasses, null, mytags.WeakAsAKitten) in {}
         }
       }
+
       // ignore
       intercept[NullPointerException] {
         new WordSpec {
@@ -467,6 +494,42 @@ class WordSpecSpec extends FunSpec with GivenWhenThen {
       intercept[NullPointerException] {
         new WordSpec {
           "hi" taggedAs(mytags.SlowAsMolasses, null, mytags.WeakAsAKitten) ignore {}
+        }
+      }
+
+      // registerTest
+      intercept[NullPointerException] {
+        new WordSpec {
+          registerTest("hi", null) {}
+        }
+      }
+      val caught3 = intercept[NullPointerException] {
+        new WordSpec {
+          registerTest("hi", mytags.SlowAsMolasses, null) {}
+        }
+      }
+      assert(caught3.getMessage == "a test tag was null")
+      intercept[NullPointerException] {
+        new WordSpec {
+          registerTest("hi", mytags.SlowAsMolasses, null, mytags.WeakAsAKitten) {}
+        }
+      }
+
+      // registerIgnoredTest
+      intercept[NullPointerException] {
+        new WordSpec {
+          registerIgnoredTest("hi", null) {}
+        }
+      }
+      val caught4 = intercept[NullPointerException] {
+        new WordSpec {
+          registerIgnoredTest("hi", mytags.SlowAsMolasses, null) {}
+        }
+      }
+      assert(caught4.getMessage == "a test tag was null")
+      intercept[NullPointerException] {
+        new WordSpec {
+          registerIgnoredTest("hi", mytags.SlowAsMolasses, null, mytags.WeakAsAKitten) {}
         }
       }
     }
@@ -809,6 +872,179 @@ class WordSpecSpec extends FunSpec with GivenWhenThen {
       assert(!k.theTestTheOtherCalled)
     }
 
+    it("should run only those registered tests selected by the tags to include and exclude sets") {
+
+      // Nothing is excluded
+      val a = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        registerTest("test this", mytags.SlowAsMolasses) { theTestThisCalled = true }
+        registerTest("test that") { theTestThatCalled = true }
+      }
+
+      import scala.language.reflectiveCalls
+
+      val repA = new TestIgnoredTrackingReporter
+      a.run(None, Args(repA))
+      assert(!repA.testIgnoredReceived)
+      assert(a.theTestThisCalled)
+      assert(a.theTestThatCalled)
+
+      // SlowAsMolasses is included, one test should be excluded
+      val b = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        registerTest("test this", mytags.SlowAsMolasses) { theTestThisCalled = true }
+        registerTest("test that") { theTestThatCalled = true }
+      }
+      val repB = new TestIgnoredTrackingReporter
+      b.run(None, Args(repB, Stopper.default, Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set()), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repB.testIgnoredReceived)
+      assert(b.theTestThisCalled)
+      assert(!b.theTestThatCalled)
+
+      // SlowAsMolasses is included, and both tests should be included
+      val c = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        registerTest("test this", mytags.SlowAsMolasses) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+      }
+      val repC = new TestIgnoredTrackingReporter
+      c.run(None, Args(repB, Stopper.default, Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set()), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repC.testIgnoredReceived)
+      assert(c.theTestThisCalled)
+      assert(c.theTestThatCalled)
+
+      // SlowAsMolasses is included. both tests should be included but one ignored
+      val d = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        registerIgnoredTest("test this", mytags.SlowAsMolasses) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+      }
+      val repD = new TestIgnoredTrackingReporter
+      d.run(None, Args(repD, Stopper.default, Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.Ignore")), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(repD.testIgnoredReceived)
+      assert(!d.theTestThisCalled)
+      assert(d.theTestThatCalled)
+
+      // SlowAsMolasses included, FastAsLight excluded
+      val e = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repE = new TestIgnoredTrackingReporter
+      e.run(None, Args(repE, Stopper.default, Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight")),
+        ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repE.testIgnoredReceived)
+      assert(!e.theTestThisCalled)
+      assert(e.theTestThatCalled)
+      assert(!e.theTestTheOtherCalled)
+
+      // An Ignored test that was both included and excluded should not generate a TestIgnored event
+      val f = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerIgnoredTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repF = new TestIgnoredTrackingReporter
+      f.run(None, Args(repF, Stopper.default, Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight")),
+        ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repF.testIgnoredReceived)
+      assert(!f.theTestThisCalled)
+      assert(f.theTestThatCalled)
+      assert(!f.theTestTheOtherCalled)
+
+      // An Ignored test that was not included should not generate a TestIgnored event
+      val g = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerIgnoredTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repG = new TestIgnoredTrackingReporter
+      g.run(None, Args(repG, Stopper.default, Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight")),
+        ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repG.testIgnoredReceived)
+      assert(!g.theTestThisCalled)
+      assert(g.theTestThatCalled)
+      assert(!g.theTestTheOtherCalled)
+
+      // No tagsToInclude set, FastAsLight excluded
+      val h = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repH = new TestIgnoredTrackingReporter
+      h.run(None, Args(repH, Stopper.default, Filter(None, Set("org.scalatest.FastAsLight")), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repH.testIgnoredReceived)
+      assert(!h.theTestThisCalled)
+      assert(h.theTestThatCalled)
+      assert(h.theTestTheOtherCalled)
+
+      // No tagsToInclude set, mytags.SlowAsMolasses excluded
+      val i = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repI = new TestIgnoredTrackingReporter
+      i.run(None, Args(repI, Stopper.default, Filter(None, Set("org.scalatest.SlowAsMolasses")), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repI.testIgnoredReceived)
+      assert(!i.theTestThisCalled)
+      assert(!i.theTestThatCalled)
+      assert(i.theTestTheOtherCalled)
+
+      // No tagsToInclude set, mytags.SlowAsMolasses excluded, TestIgnored should not be received on excluded ones
+      val j = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerIgnoredTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerIgnoredTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repJ = new TestIgnoredTrackingReporter
+      j.run(None, Args(repJ, Stopper.default, Filter(None, Set("org.scalatest.SlowAsMolasses")), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(!repI.testIgnoredReceived)
+      assert(!j.theTestThisCalled)
+      assert(!j.theTestThatCalled)
+      assert(j.theTestTheOtherCalled)
+
+      // Same as previous, except Ignore specifically mentioned in excludes set
+      val k = new WordSpec {
+        var theTestThisCalled = false
+        var theTestThatCalled = false
+        var theTestTheOtherCalled = false
+        registerIgnoredTest("test this", mytags.SlowAsMolasses, mytags.FastAsLight) { theTestThisCalled = true }
+        registerIgnoredTest("test that", mytags.SlowAsMolasses) { theTestThatCalled = true }
+        registerIgnoredTest("test the other") { theTestTheOtherCalled = true }
+      }
+      val repK = new TestIgnoredTrackingReporter
+      k.run(None, Args(repK, Stopper.default, Filter(None, Set("org.scalatest.SlowAsMolasses", "org.scalatest.Ignore")), ConfigMap.empty, None, new Tracker, Set.empty))
+      assert(repK.testIgnoredReceived)
+      assert(!k.theTestThisCalled)
+      assert(!k.theTestThatCalled)
+      assert(!k.theTestTheOtherCalled)
+    }
+
     it("should return the correct test count from its expectedTestCount method") {
 
       val a = new WordSpec {
@@ -852,6 +1088,50 @@ class WordSpecSpec extends FunSpec with GivenWhenThen {
 
       val f = new Suites(a, b, c, d, e)
       assert(f.expectedTestCount(Filter()) === 10)
+    }
+    it("should return the correct test count from its expectedTestCount method when uses registerTest and registerIgnoredTest to register tests") {
+
+      val a = new WordSpec {
+        registerTest("test this") {}
+        registerTest("test that") {}
+      }
+      assert(a.expectedTestCount(Filter()) == 2)
+
+      val b = new WordSpec {
+        registerIgnoredTest("test this") {}
+        registerTest("test that") {}
+      }
+      assert(b.expectedTestCount(Filter()) == 1)
+
+      val c = new WordSpec {
+        registerTest("test this", mytags.FastAsLight) {}
+        registerTest("test that") {}
+      }
+      assert(c.expectedTestCount(Filter(Some(Set("org.scalatest.FastAsLight")), Set())) == 1)
+      assert(c.expectedTestCount(Filter(None, Set("org.scalatest.FastAsLight"))) == 1)
+
+      val d = new WordSpec {
+        registerTest("test this", mytags.FastAsLight, mytags.SlowAsMolasses) {}
+        registerTest("test that", mytags.SlowAsMolasses) {}
+        registerTest("test the other thing") {}
+      }
+      assert(d.expectedTestCount(Filter(Some(Set("org.scalatest.FastAsLight")), Set())) == 1)
+      assert(d.expectedTestCount(Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight"))) == 1)
+      assert(d.expectedTestCount(Filter(None, Set("org.scalatest.SlowAsMolasses"))) == 1)
+      assert(d.expectedTestCount(Filter()) == 3)
+
+      val e = new WordSpec {
+        registerTest("test this", mytags.FastAsLight, mytags.SlowAsMolasses) {}
+        registerTest("test that", mytags.SlowAsMolasses) {}
+        registerIgnoredTest("test the other thing") {}
+      }
+      assert(e.expectedTestCount(Filter(Some(Set("org.scalatest.FastAsLight")), Set())) == 1)
+      assert(e.expectedTestCount(Filter(Some(Set("org.scalatest.SlowAsMolasses")), Set("org.scalatest.FastAsLight"))) == 1)
+      assert(e.expectedTestCount(Filter(None, Set("org.scalatest.SlowAsMolasses"))) == 0)
+      assert(e.expectedTestCount(Filter()) == 2)
+
+      val f = new Suites(a, b, c, d, e)
+      assert(f.expectedTestCount(Filter()) == 10)
     }
     it("should generate a TestPending message when the test body is (pending)") {
       val a = new WordSpec {
