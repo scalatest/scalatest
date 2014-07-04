ecba13c4479ba8957b479d0cd82193563c915a80
bvenners <bill@artima.com>
2014-07-04 15:32:57 -0700

    Changed how I did the explicitly stuff with XML: just add a type parameter to compressed.

--- src/test/scala/org/scalactic/XmlExplicitlySpec.scala
+++ src/test/scala/org/scalactic/XmlExplicitlySpec.scala
@@ -24,32 +24,30 @@ import scala.collection.GenTraversable
 import scala.collection.GenTraversableOnce
 import scala.xml.{Elem, Node, Text, NodeSeq}
 
-class XmlExplicitlySpec extends Spec with Matchers {
+class XmlExplicitlySpec extends Spec with Matchers with XmlNormalization {
 
   object `Xml Equality of Elems` {
 
-    def compressed: Uniformity[Elem] = XmlNormalization.compressed[Elem]
-
     def `should leave already-normalized XML alone` {
-      <summer></summer> should equal (<summer></summer>) (after being compressed)
+      <summer></summer> should equal (<summer></summer>) (after being compressed[Elem])
     }
 
     def `should zap text that is only whitespace` {
 
-      <summer> </summer> should equal (<summer></summer>) (after being compressed)
+      <summer> </summer> should equal (<summer></summer>) (after being compressed[Elem])
 
       <summer>
-       </summer> should equal (<summer></summer>) (after being compressed)
+       </summer> should equal (<summer></summer>) (after being compressed[Elem])
 
       <summer>
         <day></day>
-      </summer> should equal (<summer><day></day></summer>) (after being compressed)
+      </summer> should equal (<summer><day></day></summer>) (after being compressed[Elem])
 
       <summer><day></day></summer> should equal (
         <summer>
           <day></day>
         </summer>
-      ) (after being compressed)
+      ) (after being compressed[Elem])
 
       <summer><day>Dude!</day></summer> should equal (
         <summer>
@@ -57,39 +55,37 @@ class XmlExplicitlySpec extends Spec with Matchers {
             Dude!
           </day>
         </summer>
-      ) (after being compressed)
+      ) (after being compressed[Elem])
 
-      <div>{Text("My name is ")}{Text("Harry")}</div> should equal (<div>My name is Harry</div>) (after being compressed)
+      <div>{Text("My name is ")}{Text("Harry")}</div> should equal (<div>My name is Harry</div>) (after being compressed[Elem])
     }
   }
 
   object `Xml Equality of Nodes` {
 
-    def compressed: Uniformity[Node] = XmlNormalization.compressed[Node]
-
     def `should leave already-normalized XML alone` {
 
-      ((<summer></summer>: Node) shouldEqual (<summer></summer>)) (after being compressed)
+      ((<summer></summer>: Node) shouldEqual (<summer></summer>)) (after being compressed[Node])
 
-      ((Text("Bla"): Node) shouldEqual (Text("Bla"))) (after being compressed)
+      ((Text("Bla"): Node) shouldEqual (Text("Bla"))) (after being compressed[Node])
     }
 
     def `should zap text that is only whitespace, unless it is already a Text` {
 
-      (<summer> </summer>: Node) should equal (<summer></summer>) (after being compressed)
+      (<summer> </summer>: Node) should equal (<summer></summer>) (after being compressed[Node])
 
       (<summer>
-      </summer>: Node) should equal (<summer></summer>) (after being compressed)
+      </summer>: Node) should equal (<summer></summer>) (after being compressed[Node])
 
       (<summer>
         <day></day>
-      </summer>: Node) should equal (<summer><day></day></summer>) (after being compressed)
+      </summer>: Node) should equal (<summer><day></day></summer>) (after being compressed[Node])
 
       <summer><day></day></summer> should equal (
         <summer>
           <day></day>
         </summer>: Node
-      ) (after being compressed)
+      ) (after being compressed[Node])
 
       <summer><day>Dude!</day></summer> should equal (
         <summer>
@@ -97,41 +93,39 @@ class XmlExplicitlySpec extends Spec with Matchers {
             Dude!
           </day>
         </summer>: Node
-      ) (after being compressed)
+      ) (after being compressed[Node])
 
-      (Text("   "): Node) should equal (Text("   ")) (after being compressed)
+      (Text("   "): Node) should equal (Text("   ")) (after being compressed[Node])
 
-      (<div>{Text("My name is ")}{Text("Harry")}</div>: Node) should equal (<div>My name is Harry</div>) (after being compressed)
+      (<div>{Text("My name is ")}{Text("Harry")}</div>: Node) should equal (<div>My name is Harry</div>) (after being compressed[Node])
     }
   }
 
   object `Xml Normalization of NodeSeq` {
 
-    def compressed: Uniformity[NodeSeq] = XmlNormalization.compressed[NodeSeq]
-
     def `should leave already-normalized XML alone` {
 
-      (<summer></summer>: NodeSeq) should equal (<summer></summer>) (after being compressed)
+      (<summer></summer>: NodeSeq) should equal (<summer></summer>) (after being compressed[NodeSeq])
 
-      (Text("Bla"): NodeSeq) should equal (Text("Bla")) (after being compressed)
+      (Text("Bla"): NodeSeq) should equal (Text("Bla")) (after being compressed[NodeSeq])
     }
 
     def `should zap text that is only whitespace, unless it is already a Text` {
 
-      (<summer> </summer>: NodeSeq) should equal (<summer></summer>) (after being compressed)
+      (<summer> </summer>: NodeSeq) should equal (<summer></summer>) (after being compressed[NodeSeq])
 
       (<summer>
-      </summer>: NodeSeq) should equal (<summer></summer>) (after being compressed)
+      </summer>: NodeSeq) should equal (<summer></summer>) (after being compressed[NodeSeq])
 
       (<summer>
         <day></day>
-      </summer>: NodeSeq) should equal (<summer><day></day></summer>) (after being compressed)
+      </summer>: NodeSeq) should equal (<summer><day></day></summer>) (after being compressed[NodeSeq])
 
       <summer><day></day></summer> should equal (
         <summer>
           <day></day>
         </summer>: NodeSeq
-      ) (after being compressed)
+      ) (after being compressed[NodeSeq])
 
       <summer><day>Dude!</day></summer> should equal (
         <summer>
@@ -139,11 +133,11 @@ class XmlExplicitlySpec extends Spec with Matchers {
             Dude!
           </day>
         </summer>: NodeSeq
-      ) (after being compressed)
+      ) (after being compressed[NodeSeq])
 
-      (Text("   "): NodeSeq) should equal (Text("   ")) (after being compressed)
+      (Text("   "): NodeSeq) should equal (Text("   ")) (after being compressed[NodeSeq])
 
-      (<div>{Text("My name is ")}{Text("Harry")}</div>: NodeSeq) should equal (<div>My name is Harry</div>) (after being compressed)
+      (<div>{Text("My name is ")}{Text("Harry")}</div>: NodeSeq) should equal (<div>My name is Harry</div>) (after being compressed[NodeSeq])
     }
   }
 }
