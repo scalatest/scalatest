2a5886566ac875a5f4147b5335650d31e1e45d44
bvenners <bill@artima.com>
2014-09-18 12:33:37 -0600

    Added commented out tests for && and || on Fact.

--- src/test/scala/org/scalactic/FactSpec.scala
+++ src/test/scala/org/scalactic/FactSpec.scala
@@ -280,4 +280,232 @@ class FactResultSpec extends FreeSpec with Matchers with PrettyMethods {
       )
     }
   }
+
+/*
+  "The MatchResult obtained from and-ing two Matchers" - {
+    /*
+      scala> be > 'b' and be > 'd'
+      res0: org.scalatest.matchers.Matcher[Char] = <function1>
+    */
+    "should be lazy about constructing strings" - {
+      /*
+        scala> res0('a')
+        res1: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'b','a' was greater than 'b','a' was not greater than 'b','a' was greater than 'b',Vector(),Vector())
+      */
+      "for No && No" in {
+        val leftSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'), Prettifier.default)
+        val rightSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'), Prettifier.default)
+        val fact = leftSideNo && rightSideNo
+        fact shouldBe a [No]
+        fact.rawFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.rawNegatedFailureMessage should be (Resources("wasGreaterThan"))
+        fact.rawMidSentenceFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("wasGreaterThan"))
+        fact.failureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.negatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.midSentenceFailureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.midSentenceNegatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.failureMessageArgs should be (Vector('a', 'b'))
+        fact.negatedFailureMessageArgs should be (Vector('a', 'b'))
+      }
+
+       /*
+         scala> be > 'b' and be < 'd'
+         res4: org.scalatest.matchers.Matcher[Char] = <function1>
+
+         scala> res4('a')
+         res5: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'b','a' was greater than 'b','a' was not greater than 'b','a' was greater than 'b',Vector(),Vector())
+         
+         val m = be > 'b' and be < 'd'
+         val fact = m('a')
+       */
+      "for No && Yes" in {
+        val leftSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'), Prettifier.default)
+        val rightSideYes = Yes(Resources("wasNotLessThan"), Resources("wasLessThan"), Resources("wasNotLessThan"), Resources("wasLessThan"), Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'), Prettifier.default)
+        val fact = leftSideNo && rightSideYes
+        fact shouldBe a [No]
+        fact.rawFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.rawNegatedFailureMessage should be (Resources("wasGreaterThan"))
+        fact.rawMidSentenceFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("wasGreaterThan"))
+        fact.failureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.negatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.midSentenceFailureMessage should be (Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.midSentenceNegatedFailureMessage should be (Resources("wasGreaterThan", 'a'.pretty, 'b'.pretty))
+        fact.failureMessageArgs should be (Vector('a', 'b'))
+        fact.negatedFailureMessageArgs should be (Vector('a', 'b'))
+      }
+
+        /*
+          scala> res0('c')
+          res2: org.scalatest.matchers.MatchResult = MatchResult(false,'c' was greater than 'b', but 'c' was not greater than 'd','c' was greater than 'b', and 'c' was greater than 'd','c' was greater than 'b', but 'c' was not greater than 'd','c' was greater than 'b', and 'c' was greater than 'd',Vector(),Vector())
+
+          val left = be > 'b'
+          val right = be > 'd'
+          val m = left and right
+          val fact = m('c')
+        */
+      "for Yes && No" in {
+        val leftSideYes = Yes(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('c', 'b'),Vector('c', 'b'),Vector('c', 'b'),Vector('c', 'b'), Prettifier.default)
+        val rightSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('c', 'd'),Vector('c', 'd'),Vector('c', 'd'),Vector('c', 'd'), Prettifier.default)
+        val fact = leftSideYes && rightSideNo
+        fact shouldBe a [No]
+        fact.rawFailureMessage should be (Resources("commaBut"))
+        fact.rawNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.rawMidSentenceFailureMessage should be (Resources("commaBut"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.failureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'c'.pretty, 'd'.pretty)))
+        fact.negatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'c'.pretty, 'd'.pretty)))
+        fact.midSentenceFailureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'c'.pretty, 'd'.pretty)))
+        fact.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'c'.pretty, 'd'.pretty)))
+        fact.failureMessageArgs should be (Vector(NegatedFailureMessage(left('c')), MidSentenceFailureMessage(right('c'))))
+        fact.negatedFailureMessageArgs should be (Vector(NegatedFailureMessage(left('c')), MidSentenceNegatedFailureMessage(right('c'))))
+        fact.midSentenceFailureMessageArgs should be (Vector(MidSentenceNegatedFailureMessage(left('c')), MidSentenceFailureMessage(right('c'))))
+        fact.midSentenceNegatedFailureMessageArgs should be (Vector(MidSentenceNegatedFailureMessage(left('c')), MidSentenceNegatedFailureMessage(right('c'))))
+      }
+
+        /*
+          scala> res0('e')
+          res3: org.scalatest.matchers.MatchResult = MatchResult(true,'e' was greater than 'b', but 'e' was not greater than 'd','e' was greater than 'b', and 'e' was greater than 'd','e' was greater than 'b', but 'e' was not greater than 'd','e' was greater than 'b', and 'e' was greater than 'd',Vector(),Vector())
+
+          val left = be > 'b'
+          val right = be > 'd'
+          val m = left and right
+          val fact = m('e')
+        */
+      "for Yes && Yes" in {
+        val leftSideYes = Yes(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('e', 'b'),Vector('e', 'b'),Vector('e', 'b'),Vector('e', 'b'), Prettifier.default)
+        val rightSideYes = Yes(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('e', 'd'),Vector('e', 'd'),Vector('e', 'd'),Vector('e', 'd'), Prettifier.default)
+        val fact = leftSideYes && rightSideYes
+        fact shouldBe a [No]
+        fact.rawFailureMessage should be (Resources("commaBut"))
+        fact.rawNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.rawMidSentenceFailureMessage should be (Resources("commaBut"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.failureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'e'.pretty, 'd'.pretty)))
+        fact.negatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'e'.pretty, 'd'.pretty)))
+        fact.midSentenceFailureMessage should be (Resources("commaBut", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'e'.pretty, 'd'.pretty))) 
+        fact.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'e'.pretty, 'd'.pretty)))
+        fact.failureMessageArgs should be (Vector(NegatedFailureMessage(left('e')), MidSentenceFailureMessage(right('e'))))
+        fact.negatedFailureMessageArgs should be (Vector(NegatedFailureMessage(left('e')), MidSentenceNegatedFailureMessage(right('e'))))
+        fact.midSentenceFailureMessageArgs should be (Vector(MidSentenceNegatedFailureMessage(left('e')), MidSentenceFailureMessage(right('e'))))
+        fact.midSentenceNegatedFailureMessageArgs should be (Vector(MidSentenceNegatedFailureMessage(left('e')), MidSentenceNegatedFailureMessage(right('e'))))
+      }
+    }
+  }
+  "The MatchResult obtained from or-ing two Matchers" - {
+    /*
+      scala> be > 'b' or be > 'd'
+      res0: org.scalatest.matchers.Matcher[Char] = <function1>
+    */
+    "should be lazy about constructing strings" - {
+
+      /*
+      scala> res0('a')
+      res1: org.scalatest.matchers.MatchResult = MatchResult(false,'a' was not greater than 'b', and 'a' was not greater than 'd','a' was not greater than 'b', and 'a' was greater than 'd','a' was not greater than 'b', and 'a' was not greater than 'd','a' was not greater than 'b', and 'a' was greater than 'd',Vector(),Vector())
+
+      val left = be > 'b'
+      val right = be > 'd'
+      val m = left or right
+      val fact = m('a')
+      */
+      "for No || No" in {
+        val leftSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'), Prettifier.default)
+        val rightSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'), Prettifier.default)
+        val fact = leftSideNo || rightSideNo
+        fact shouldBe a [No]
+        fact.rawFailureMessage should be (Resources("commaAnd"))
+        fact.rawNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.rawMidSentenceFailureMessage should be (Resources("commaAnd"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.failureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'a'.pretty, 'd'.pretty)))
+        fact.negatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'a'.pretty, 'd'.pretty)))
+        fact.midSentenceFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotGreaterThan", 'a'.pretty, 'd'.pretty)))
+        fact.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasGreaterThan", 'a'.pretty, 'd'.pretty)))
+        fact.failureMessageArgs should be (Vector(FailureMessage(left('a')), MidSentenceFailureMessage(right('a'))))
+        fact.negatedFailureMessageArgs should be (Vector(FailureMessage(left('a')), MidSentenceNegatedFailureMessage(right('a'))))
+        fact.midSentenceFailureMessageArgs should be (Vector(MidSentenceFailureMessage(left('a')), MidSentenceFailureMessage(right('a'))))
+        fact.midSentenceNegatedFailureMessageArgs should be (Vector(MidSentenceFailureMessage(left('a')), MidSentenceNegatedFailureMessage(right('a'))))
+       }
+
+      /*
+        scala> be > 'b' or be < 'd'
+        res4: org.scalatest.matchers.Matcher[Char] = <function1>
+
+        scala> res4('a')
+        res5: org.scalatest.matchers.MatchResult = MatchResult(true,'a' was not greater than 'b', and 'a' was not less than 'd','a' was not greater than 'b', and 'a' was less than 'd','a' was not greater than 'b', and 'a' was not less than 'd','a' was not greater than 'b', and 'a' was less than 'd',Vector(),Vector())
+
+        val left = be > 'b'
+        val right = be < 'd'
+        val m = left or right
+        val fact = m('a')
+      */
+      "for No || Yes" in {
+        val leftSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'),Vector('a', 'b'), Prettifier.default)
+        val rightSideYes = Yes(Resources("wasNotLessThan"), Resources("wasLessThan"), Resources("wasNotLessThan"), Resources("wasLessThan"), Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'),Vector('a', 'd'), Prettifier.default)
+        val fact = leftSideNo || rightSideYes
+        fact shouldBe a [No]
+        fact.rawFailureMessage should be (Resources("commaAnd"))
+        fact.rawNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.rawMidSentenceFailureMessage should be (Resources("commaAnd"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("commaAnd"))
+        fact.failureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotLessThan", 'a'.pretty, 'd'.pretty)))
+        fact.negatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasLessThan", 'a'.pretty, 'd'.pretty)))
+        fact.midSentenceFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasNotLessThan", 'a'.pretty, 'd'.pretty)))
+        fact.midSentenceNegatedFailureMessage should be (Resources("commaAnd", Resources("wasNotGreaterThan", 'a'.pretty, 'b'.pretty), Resources("wasLessThan", 'a'.pretty, 'd'.pretty)))
+        fact.failureMessageArgs should be (Vector(FailureMessage(left('a')), MidSentenceFailureMessage(right('a'))))
+        fact.negatedFailureMessageArgs should be (Vector(FailureMessage(left('a')), MidSentenceNegatedFailureMessage(right('a'))))
+        fact.midSentenceFailureMessageArgs should be (Vector(MidSentenceFailureMessage(left('a')), MidSentenceFailureMessage(right('a'))))
+        fact.midSentenceNegatedFailureMessageArgs should be (Vector(MidSentenceFailureMessage(left('a')), MidSentenceNegatedFailureMessage(right('a'))))
+      }
+
+      /*
+      scala> res0('c')
+      res2: org.scalatest.matchers.MatchResult = MatchResult(true,'c' was greater than 'b','c' was not greater than 'b','c' was greater than 'b','c' was not greater than 'b',Vector(),Vector())
+
+      val m = be > 'b' or be > 'd'
+      val fact = m('c')
+      */
+      "for Yes || No" in {
+        val leftSideYes = Yes(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('c', 'b'),Vector('c', 'b'),Vector('c', 'b'),Vector('c', 'b'), Prettifier.default)
+        val rightSideNo = No(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('c', 'd'),Vector('c', 'd'),Vector('c', 'd'),Vector('c', 'd'), Prettifier.default)
+        val fact = leftSideYes || rightSideNo
+        fact shouldBe a [Yes]
+        fact.rawFailureMessage should be (Resources("wasGreaterThan"))
+        fact.rawNegatedFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.rawMidSentenceFailureMessage should be (Resources("wasGreaterThan"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.failureMessage should be (Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty))
+        fact.negatedFailureMessage should be (Resources("wasNotGreaterThan", 'c'.pretty, 'b'.pretty))
+        fact.midSentenceFailureMessage should be (Resources("wasGreaterThan", 'c'.pretty, 'b'.pretty))
+        fact.midSentenceNegatedFailureMessage should be (Resources("wasNotGreaterThan", 'c'.pretty, 'b'.pretty))
+        fact.failureMessageArgs should be (Vector('c', 'b'))
+        fact.negatedFailureMessageArgs should be (Vector('c', 'b'))
+      }
+
+      /*
+      scala> res0('e')
+      res3: org.scalatest.matchers.MatchResult = MatchResult(true,'e' was greater than 'b','e' was not greater than 'b','e' was greater than 'b','e' was not greater than 'b',Vector(),Vector())
+       val m = be > 'b' or be > 'd'
+       val fact = m('e')
+      */
+      "for Yes || Yes" in {
+        val leftSideYes = Yes(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('e', 'b'),Vector('e', 'b'),Vector('e', 'b'),Vector('e', 'b'), Prettifier.default)
+        val rightSideYes = Yes(Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Resources("wasNotGreaterThan"), Resources("wasGreaterThan"), Vector('e', 'd'),Vector('e', 'd'),Vector('e', 'd'),Vector('e', 'd'), Prettifier.default)
+        val fact = leftSideYes || rightSideYes
+        fact shouldBe a [Yes]
+        fact.rawFailureMessage should be (Resources("wasGreaterThan"))
+        fact.rawNegatedFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.rawMidSentenceFailureMessage should be (Resources("wasGreaterThan"))
+        fact.rawMidSentenceNegatedFailureMessage should be (Resources("wasNotGreaterThan"))
+        fact.failureMessage should be (Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty))
+        fact.negatedFailureMessage should be (Resources("wasNotGreaterThan", 'e'.pretty, 'b'.pretty))
+        fact.midSentenceFailureMessage should be (Resources("wasGreaterThan", 'e'.pretty, 'b'.pretty))
+        fact.midSentenceNegatedFailureMessage should be (Resources("wasNotGreaterThan", 'e'.pretty, 'b'.pretty))
+        fact.failureMessageArgs should be (Vector('e', 'b'))
+        fact.negatedFailureMessageArgs should be (Vector('e', 'b'))
+      }
+    }
+  }
+*/
 }
