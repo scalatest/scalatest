d7db39933dcb80d19e65717dba21e33c0b98cf65
cheeseng <cheeseng@amaseng.com>
2014-09-24 11:41:38 +0800

    Eliminates race conditions in Status and AsyncOutcome.

--- src/main/scala/org/scalatest/AsyncOutcome.scala
+++ src/main/scala/org/scalatest/AsyncOutcome.scala
@@ -1,9 +1,12 @@
 
 package org.scalatest
 
+import scala.concurrent.duration.Duration.Infinite
 import scala.util.{Success, Try, Failure}
 import scala.concurrent.{ExecutionContext, Future, Await}
 import scala.concurrent.duration._
+import java.util.concurrent.ConcurrentLinkedQueue
+import collection.JavaConverters._
 
 trait AsyncOutcome {
   def onComplete(f: Try[Outcome] => Unit)
@@ -26,22 +29,38 @@ case class PastOutcome(past: Outcome) extends AsyncOutcome {
 }
 
 case class FutureOutcome(future: Future[Outcome])(implicit ctx: ExecutionContext) extends AsyncOutcome {
+
+  private final val queue = new ConcurrentLinkedQueue[Try[Outcome] => Unit]
+  private final val status = new ScalaTestStatefulStatus
+  future.onComplete {
+    case Success(result) =>
+      for (f <- queue.iterator.asScala)
+        f(Success(result))
+      status.setCompleted()
+
+    case Failure(ex) =>
+      for (f <- queue.iterator.asScala)
+        f(Failure(ex))
+      status.setFailed()
+      status.setCompleted()
+  }
+
   def onComplete(f: Try[Outcome] => Unit) = {
-    future.onComplete {
-      case Success(result) => f(new Success(result))
-      case Failure(ex) => f(new Failure(ex))
+    var executeLocally = false
+    synchronized {
+      if (!future.isCompleted)
+        queue.add(f)
+      else
+        executeLocally = true
+    }
+    if (executeLocally) {
+      future.value.get match {
+        case Success(result) => f(new Success(result))
+        case Failure(ex) => f(new Failure(ex))
+      }
     }
   }
-  def toStatus: Status = {
-    val status = new ScalaTestStatefulStatus
-    future.onComplete {
-      case Success(result) => status.setCompleted()
-      case Failure(ex) =>
-        status.setFailed()
-        status.setCompleted()
-    }
-    status
-  }
-  def toOutcome: Outcome = Await.result(future, Duration(1000, MILLISECONDS))
+  def toStatus: Status = status
+  def toOutcome: Outcome = Await.result(future, Duration.Inf)
   def toFutureOutcome(implicit ctx: ExecutionContext): Future[Outcome] = future
 }
