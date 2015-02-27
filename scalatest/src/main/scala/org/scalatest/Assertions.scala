15ebe20bc57a93877788a5d67567a68e59919e38
Landlocked Surfer <landlocked.surfer@yahoo.com>
2015-02-27 17:35:09 +0100

    fixed typo in documentation.

--- scalatest/src/main/scala/org/scalatest/Assertions.scala
+++ scalatest/src/main/scala/org/scalatest/Assertions.scala
@@ -123,7 +123,7 @@ import exceptions.TestFailedException
  *
  * <pre class="stHighlight">
  * val attempted = 2
- * assert(attempted == 1, "Execution was attempted " + left + " times instead of 1 time")
+ * assert(attempted == 1, "Execution was attempted " + attempted + " times instead of 1 time")
  * </pre>
  *
  * <p>
