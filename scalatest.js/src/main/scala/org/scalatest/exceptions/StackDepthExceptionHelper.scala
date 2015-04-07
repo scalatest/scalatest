2c85063037954328c8984b0f40856aa55b8cef61
cheeseng <cheeseng@amaseng.com>
2015-04-07 20:53:21 +0800

    Experimental StackDepthExceptionHelper implementation for scala.js.

--- scalatest.js/src/main/scala/org/scalatest/exceptions/StackDepthExceptionHelper.scala
+++ scalatest.js/src/main/scala/org/scalatest/exceptions/StackDepthExceptionHelper.scala
@@ -2,8 +2,10 @@ package org.scalatest.exceptions
 
 private[scalatest] object StackDepthExceptionHelper {
 
-  def getStackDepth(stackTrace: Array[StackTraceElement], fileName: String, methodName: String, adjustment: Int = 0): Int =
-    0
+  def getStackDepth(stackTraces: Array[StackTraceElement], fileName: String, methodName: String, adjustment: Int = 0): Int = {
+    val preceedingHttps = stackTraces.takeWhile(_.getFileName.startsWith("https://"))
+    preceedingHttps.length + stackTraces.drop(preceedingHttps.length).takeWhile(_.getFileName.startsWith("file:/")).length - 1
+  }
 
   def getStackDepthFun(fileName: String, methodName: String, adjustment: Int = 0): (StackDepthException => Int) = { sde =>
     getStackDepth(sde.getStackTrace, fileName, methodName, adjustment)
