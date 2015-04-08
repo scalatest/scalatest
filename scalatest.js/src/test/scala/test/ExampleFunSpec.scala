cc4d8236f55bd58c9cc5b2db3edffad1a5e1dbef
cheeseng <cheeseng@amaseng.com>
2015-04-08 16:18:49 +0800

    Rewritten ExampleFunSuite and ExampleFunSpec using example given from scalatest.org.

--- scalatest.js/src/test/scala/test/ExampleFunSpec.scala
+++ scalatest.js/src/test/scala/test/ExampleFunSpec.scala
@@ -1,17 +1,20 @@
 package test
 
-import org.scalatest._
+import org.scalatest.FunSpec
 
 class ExampleFunSpec extends FunSpec {
 
-  describe("a scope") {
+  describe("A Set") {
+    describe("when empty") {
+      it("should have size 0") {
+        assert(Set.empty.size == 0)
+      }
 
-    it("feature 1") {}
-
-    it("feature 2") {}
-
-    it("feature 3") {}
-
+      it("should produce NoSuchElementException when head is invoked") {
+        intercept[NoSuchElementException] {
+          Set.empty.head
+        }
+      }
+    }
   }
-
 }
