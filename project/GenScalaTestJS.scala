ce3f1b2f62a933463870810047f12420735260de
cheeseng <cheeseng@amaseng.com>
2015-06-01 23:56:27 +0800

    Tests under org.scalatest.time passed in js env.

--- project/GenScalaTestJS.scala
+++ project/GenScalaTestJS.scala
@@ -510,6 +510,7 @@ object GenScalaTestJS {
     copyDir("scalatest-test/src/test/scala/org/scalatest/path", "org/scalatest/path", targetDir, List.empty) ++
     copyDir("scalatest-test/src/test/scala/org/scalatest/prop", "org/scalatest/prop", targetDir, List.empty) ++
     copyDir("scalatest-test/src/test/scala/org/scalatest/matchers", "org/scalatest/matchers", targetDir, List.empty) ++
+    copyDir("scalatest-test/src/test/scala/org/scalatest/time", "org/scalatest/time", targetDir, List.empty) ++
     copyFiles("scalatest-test/src/test/scala/org/scalatest/words", "org/scalatest/words",
       List(
         "ResultOfAllElementsOfApplicationSpec.scala",
