79e97c96bf75d5334310bddbac5bb7448b437c1b
George Berger <github.ecto@xoxy.net>
2015-10-01 15:01:08 -0700

    Add tests to MatcherStackDepthSpec

--- scalatest-test/src/test/scala/org/scalatest/MatcherStackDepthSpec.scala
+++ scalatest-test/src/test/scala/org/scalatest/MatcherStackDepthSpec.scala
@@ -24,4 +24,123 @@ class MatcherStackDepthSpec extends FunSuite with Matchers {
     }
     e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
   }
+
+  test("0 should === (1)") {
+    val e = intercept[exceptions.TestFailedException] {
+      0 should === (1)
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test("0 should be (1)") {
+    val e = intercept[exceptions.TestFailedException] {
+      0 should be (1)
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test("0 shouldEqual (1)") {
+    val e = intercept[exceptions.TestFailedException] {
+      0 shouldEqual (1)
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test("0 shouldBe (1)") {
+    val e = intercept[exceptions.TestFailedException] {
+      0 shouldBe (1)
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should have length (1)""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should have length (1)
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should have size (1)""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should have size (1)
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should startWith ("def")""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should startWith ("def")
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should endWith ("def")""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should endWith ("def")
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should include ("def")""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should include ("def")
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should startWith regex "def"""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should startWith regex "def"
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should endWith regex "def"""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should endWith regex "def"
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should include regex "def"""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should include regex "def"
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should fullyMatch regex "def"""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should fullyMatch regex "def"
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should startWith regex ("ab(c*)" withGroups ("ff"))""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should startWith regex ("ab(c*)" withGroups ("ff"))
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should endWith regex ("ab(c*)") withGroups ("ff"))""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should endWith regex ("ab(c*)" withGroups ("ff"))
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should include regex ("ab(c*)" withGroups ("ff"))""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should include regex ("ab(c*)" withGroups ("ff"))
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
+
+  test(""""abc" should fullyMatch regex ("ab(c*)" withGroups ("ff"))""") {
+    val e = intercept[exceptions.TestFailedException] {
+      "abc" should fullyMatch regex ("ab(c*)" withGroups ("ff"))
+    }
+    e.failedCodeLineNumber should be (Some(thisLineNumber - 2))
+  }
 }
