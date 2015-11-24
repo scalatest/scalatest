a938f53e7495d82d08b42d146acbd9941b86d074
bvenners <bill@artima.com>
2015-11-24 10:35:13 -0800

    Replaced composingwithfixture example in AsyncFunSuite with simulated actor-based one.

--- scalatest/src/main/scala/org/scalatest/AsyncFunSuite.scala
+++ scalatest/src/main/scala/org/scalatest/AsyncFunSuite.scala
@@ -1176,47 +1176,65 @@ package org.scalatest
  * import scala.concurrent.Future
  * import scala.concurrent.ExecutionContext
  * 
- * class ThreadSafeListBufferOfString {
- *   private final val buf = ListBuffer.empty[String]
- *   def += (s: String): Unit = synchronized { buf += s }
- *   def toList: List[String] = synchronized { buf.toList }
- *   def clear(): Unit = synchronized { buf.clear() }
- *   def isEmpty: Boolean = synchronized { buf.isEmpty }
+ * // Defining actor messages
+ * sealed abstract class StringOp
+ * case object Clear extends StringOp
+ * case class Append(value: String) extends StringOp
+ * case object GetValue
+ * 
+ * class StringBuilderActor { // Simulating an actor
+ *   private final val sb = new StringBuilder
+ *   def !(op: StringOp): Unit =
+ *     synchronized {
+ *       op match {
+ *         case Append(value) =&gt; sb.append(value)
+ *         case Clear =&gt; sb.clear()
+ *       }
+ *     }
+ *   def ?(get: GetValue.type)(implicit c: ExecutionContext): Future[String] =
+ *     Future {
+ *       synchronized { sb.toString }
+ *     }
  * }
  * 
- * class ThreadSafeStringBuilder {
- *   private final val bldr = new StringBuilder
- *   def append(s: String): Unit =
+ * class StringBufferActor {
+ *   private final val buf = ListBuffer.empty[String]
+ *   def !(op: StringOp): Unit =
  *     synchronized {
- *       bldr.append(s)
+ *       op match {
+ *         case Append(value) =&gt; buf += value
+ *         case Clear =&gt; buf.clear()
+ *       }
  *     }
- *   def clear(): Unit = synchronized { bldr.clear() }
- *   override def toString = synchronized { bldr.toString }
+ *   def ?(get: GetValue.type)(implicit c: ExecutionContext): Future[List[String]] =
+ *     Future {
+ *       synchronized { buf.toList }
+ *     }
  * }
  * 
  * trait Builder extends AsyncSuiteMixin { this: AsyncSuite =&gt;
  * 
- *   final val builder = new ThreadSafeStringBuilder
+ *   final val builderActor = new StringBuilderActor
  * 
  *   abstract override def withAsyncFixture(test: NoArgAsyncTest) = {
- *     builder.append("ScalaTest is ")
+ *     builderActor ! Append("ScalaTest is ")
  *     withCleanup {
  *       super.withAsyncFixture(test) // To be stackable, must call super.withAsyncFixture
  *     } {
- *       builder.clear()
+ *       builderActor ! Clear
  *     }
  *   }
  * }
  * 
  * trait Buffer extends AsyncSuiteMixin { this: AsyncSuite =&gt;
  * 
- *   final val buffer = new ThreadSafeListBufferOfString
+ *   final val bufferActor = new StringBufferActor
  * 
  *   abstract override def withAsyncFixture(test: NoArgAsyncTest) = {
  *     withCleanup {
  *       super.withAsyncFixture(test) // To be stackable, must call super.withAsyncFixture
  *     } {
- *       buffer.clear()
+ *       bufferActor ! Clear
  *     }
  *   }
  * }
@@ -1226,21 +1244,27 @@ package org.scalatest
  *   implicit val executionContext = ExecutionContext.Implicits.global
  * 
  *   test("Testing should be easy") {
- *     Future {
- *       builder.append("easy!")
- *       assert(builder.toString === "ScalaTest is easy!")
- *       assert(buffer.isEmpty)
- *       buffer += "sweet"
+ *     builderActor ! Append("easy!")
+ *     val futureString = builderActor ? GetValue
+ *     val futureList = bufferActor ? GetValue
+ *     val futurePair: Future[(String, List[String])] = futureString zip futureList
+ *     futurePair map { case (str, lst) =&gt;
+ *       assert(str === "ScalaTest is easy!")
+ *       assert(lst.isEmpty)
+ *       bufferActor ! Append("sweet")
  *       succeed
  *     }
  *   }
  * 
  *   test("Testing should be fun") {
- *     Future {
- *       builder.append("fun!")
- *       assert(builder.toString === "ScalaTest is fun!")
- *       assert(buffer.isEmpty)
- *       buffer += "clear"
+ *     builderActor ! Append("fun!")
+ *     val futureString = builderActor ? GetValue
+ *     val futureList = bufferActor ? GetValue
+ *     val futurePair: Future[(String, List[String])] = futureString zip futureList
+ *     futurePair map { case (str, lst) =&gt;
+ *       assert(str === "ScalaTest is fun!")
+ *       assert(lst.isEmpty)
+ *       bufferActor ! Append("sweet")
  *       succeed
  *     }
  *   }
