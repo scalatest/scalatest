6ecffad8fe2a69854eabe1248b38753774ae34fc
cheeseng <cheeseng@amaseng.com>
2015-12-09 00:20:04 +0800

    Added examples for AsyncFlatSpec.

---
+++ examples/src/test/scala/org/scalatest/examples/asyncflatspec/noargasynctest/ExampleSpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2001-2015 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.examples.asyncflatspec.noargasynctest
+
+import java.io.File
+import org.scalatest._
+import scala.concurrent.Future
+
+class ExampleSpec extends AsyncFlatSpec {
+
+  override def withAsyncFixture(test: NoArgAsyncTest) = {
+
+    val futureOutcome = super.withAsyncFixture(test)
+
+    futureOutcome onSuccess {
+      case _: Failed =>
+        val currDir = new File(".")
+        val fileNames = currDir.list()
+        println("Dir snapshot: " + fileNames.mkString(", "))
+    }
+
+    futureOutcome
+  }
+
+  def addSoon(addends: Int*): Future[Int] = Future { addends.sum }
+    
+  "This test" should "succeed" in {
+    addSoon(1, 1) map { sum => assert(sum == 2) }
+  }
+
+  it should "fail" in {
+    addSoon(1, 1) map { sum => assert(sum == 3) }
+  }
+  
+}
+
