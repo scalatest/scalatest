29ee781b67592963c7dba14cc399b26338d2c381
bvenners <bill@artima.com>
2016-05-05 20:50:05 -0700

    Severed inheritance relationship of StringVerbBlockRegistration and ((String, String, source.Position, () => Unit) => Unit). Because I had this as an implicit val, that actually served as an implicit conversion because it was a function type!

--- scalatest/src/main/scala/org/scalatest/words/StringVerbBlockRegistration.scala
+++ scalatest/src/main/scala/org/scalatest/words/StringVerbBlockRegistration.scala
@@ -18,10 +18,6 @@ package org.scalatest.words
 import org.scalatest._
 import org.scalactic._
 
-// Used to make an implicit conversion more specific. If there were ever another
-// implicit (String, String, () => Unit) lying around in scope, it would clash with
-// if the implicit was just based on that more general type. This is used in
-// ShouldVerb, MustVerb, and CanVerb.
 /**
  * Class that provides a role-specific type for an implicit conversion used to support
  * the registration of subject descriptions in <code>WordSpec</code> and <code>fixture.WordSpec</code>.
@@ -37,14 +33,11 @@ import org.scalactic._
  * </pre>
  *
  * This <code>should</code> method, which is provided in <code>ShouldVerb</code>, needs an implicit parameter
- * of type <code>(String, String, () => Unit) => Unit</code>. Because the required type has no ScalaTest-specific
- * types in it, it is possible that another implicit parameter of that same type could be in scope, which would
- * cause a compile failure. Requiring an implicit parameter of the more specific <code>StringVerbBlockRegistration</code>,
- * which simply extends the needed type, <code>(String, String, () => Unit) => Unit</code>, avoids this potential conflict.
+ * of type <code>StringVerbBlockRegistration</code>.
  *
  * @author Bill Venners
  */
-abstract class StringVerbBlockRegistration extends ((String, String, source.Position, () => Unit) => Unit) {
+abstract class StringVerbBlockRegistration {
 
   /**
    * Registers a subject description in <code>WordSpec</code> and <code>fixture.WordSpec</code>.
@@ -60,6 +53,6 @@ abstract class StringVerbBlockRegistration extends ((String, String, source.Posi
    * </pre>
    *
    */
-  def apply(string: String, verb: String, pos: source.Position, block: () => Unit)
+  def apply(string: String, verb: String, pos: source.Position, block: () => Unit): Unit
 }
 
