6b8de72e15858d562c7313a6c0d8f1c9d6c624c2
cheeseng <cheeseng@amaseng.com>
2016-06-06 20:03:04 +0800

    Bump up scala-js version to 0.6.9 and fixed linking problem when launching ScalaTest tests.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -184,7 +184,7 @@ object ScalatestBuild extends Build {
 
   def scalatestJSLibraryDependencies =
     Seq(
-      "org.scala-js" %% "scalajs-test-interface" % "0.6.8"
+      "org.scala-js" %% "scalajs-test-interface" % "0.6.9"
     )
 
   def scalatestTestOptions =
