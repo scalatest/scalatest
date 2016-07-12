73b7f247e99c29d7e7d571a9b3888a75dfe6078b
bvenners <bill@artima.com>
2016-07-12 07:15:51 -0700

    Bumped version to 3.0.0.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -21,7 +21,7 @@ object ScalatestBuild extends Build {
   // > ++ 2.10.5
   val buildScalaVersion = "2.11.8"
 
-  val releaseVersion = "3.0.0-RC4"
+  val releaseVersion = "3.0.0"
 
   val scalacheckVersion = "1.13.1"
 
