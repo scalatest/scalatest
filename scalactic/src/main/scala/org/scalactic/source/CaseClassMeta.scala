197654f417035f22a3a8d7dec51f6c5607ce421a
cheeseng <cheeseng@amaseng.com>
2016-07-25 16:18:30 +0800

    Added initial version of CaseClassMeta, not working under Scala 2.10 yet.

---
+++ scalactic/src/main/scala/org/scalactic/source/CaseClassMeta.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2001-2016 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalactic.source
+
+trait CaseClassMeta {
+
+  def caseAccessorNames: scala.collection.immutable.IndexedSeq[String]
+
+  def value(name: String): Any
+
+}
+
+object CaseClassMeta {
+
+  def apply(value: Any): CaseClassMeta = {
+    import reflect.runtime.universe._
+
+    def isCaseAccessor(s: Symbol): Boolean = {
+      s match {
+        case m: MethodSymbol if m.isCaseAccessor => true
+        case _ => false
+      }
+    }
+
+    val typeMirror = runtimeMirror(value.getClass.getClassLoader)
+    val instanceMirror = typeMirror.reflect(value)
+    val symbol: ClassSymbol = instanceMirror.symbol
+
+    if (symbol.isCaseClass) {
+      new CaseClassMeta {
+
+        val caseAccessorSymbols = symbol.info.declarations.filter(isCaseAccessor)
+
+        lazy val caseAccessorNames = {
+          caseAccessorSymbols.map(_.name.toString).toVector
+        }
+
+        def value(name: String): Any = {
+          caseAccessorSymbols.find(s => s.name.toString == name) match {
+            case Some(fieldSymbol) =>
+              val fieldMirror = instanceMirror.reflectField(fieldSymbol.asTerm)
+              fieldMirror.get
+
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not a case accessor for this instance of case class.")
+          }
+        }
+      }
+    }
+    else
+      throw new IllegalArgumentException(symbol.fullName + " is not a case class.")
+  }
+
+}
