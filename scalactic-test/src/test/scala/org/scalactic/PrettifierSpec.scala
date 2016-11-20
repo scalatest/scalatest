57b23da8a6cc5dbbc8647638c231eda177e5ac21
bvenners <bill@artima.com>
2016-11-20 18:50:03 -0500

    Added an apply method to Prettifier that takes two Any arguments and returns a PrettyPair, which I also just added. PrettyPair has two prettifier objects, which were found to be unequal, so the idea is that this new apply method on Prettifier will use the differences to help decide how to prettify them. For example in strings we could put the []'s in the right spots. Or for long strings we can shorten them and highlight just the part (or at least the first part) that is different. And there's also a hint string in PrettyPair, that can be used to give info about the difference.

--- scalactic-test/src/test/scala/org/scalactic/PrettifierSpec.scala
+++ scalactic-test/src/test/scala/org/scalactic/PrettifierSpec.scala
@@ -54,6 +54,19 @@ class PrettifierSpec extends FunSpec with Matchers {
       myLittlePretty(()) should be ("<(), the Unit value>")
       myLittlePretty(List("1", "2", "3")) should be ("List(\"1\", \"2\", \"3\")")
     }
+    it("should by default offer an apply method that takes two args that returns a PrettyPair whose left and right are prettified by the one-arg apply and hint is None.") {
+    
+      case class Yell(secret: String)
+      val myLittlePretty =
+        new Prettifier {
+          def apply(o: Any) =
+            o match {
+              case Yell(secret) => secret.toUpperCase + "!!!"
+              case _ => Prettifier.default(o)
+            }
+        }
+      myLittlePretty(Yell("I like fruit loops"), Yell("I like raisin bran")) shouldBe PrettyPair("I LIKE FRUIT LOOPS!!!", "I LIKE RAISIN BRAN!!!", None)
+    }
   }
 
   describe("the basic Prettifier") {
