24d439fbe140956ae6c081e5e47ec4a68612d8bc
cheeseng <cheeseng@amaseng.com>
2016-12-06 23:20:47 +0800

    Added ObjectMetaSpec.

---
+++ scalactic-test/src/test/scala/org/scalactic/source/ObjectMetaSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2001-2016 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalactic.source
+
+import org.scalatest._
+
+class ObjectMetaSpec extends FunSpec with Matchers {
+
+  case class Person(name: String, age: Int) {
+    val otherField = "test other field"
+  }
+
+  describe("ObjectMeta") {
+
+    it("should extract case class attribute names correctly") {
+      ObjectMeta(Person("test", 33)).fieldNames should contain theSameElementsAs Set("name", "age", "otherField")
+    }
+
+    it("should extract dynamically field value correctly") {
+      val meta = ObjectMeta(Person("test", 33))
+      meta.value("name") shouldBe "test"
+      meta.value("age") shouldBe 33
+      meta.value("otherField") shouldBe "test other field"
+    }
+
+    it("should throw IllegalArgumentException when invalid attribute name is used to retrieve value") {
+      val meta = ObjectMeta(Person("test", 33))
+      val e = intercept[IllegalArgumentException] {
+        meta.value("invalid")
+      }
+      e.getMessage shouldBe "'invalid' is not attribute for this instance."
+    }
+  }
+}
