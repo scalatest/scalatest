1ccc8c14d1279db428e29c3ab4f8b4eee661c13e
cheeseng <cheeseng@amaseng.com>
2016-12-07 21:46:47 +0800

    Fixed broken genGenTests due to oneOf method refactoring to org.scalatest.prop.specificValues.

--- project/GenGen.scala
+++ project/GenGen.scala
@@ -1108,7 +1108,7 @@ import org.scalatest.exceptions.GeneratorDrivenPropertyCheckFailedException
 
 val generatorSuitePostamble = """
   val famousLastWords = for {
-    s <- Generator.oneOf("the", "program", "compiles", "therefore", "it", "should", "work")
+    s <- org.scalatest.prop.specificValues("the", "program", "compiles", "therefore", "it", "should", "work")
   } yield s
 
   val sevenEleven: Generator[String] =
