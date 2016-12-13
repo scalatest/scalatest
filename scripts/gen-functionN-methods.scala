3b90e8190d83682e6ef3c88860624d874c13099b
bvenners <bill@artima.com>
2016-12-13 12:33:39 -0800

    Modified the code generator script for functionN generators and associated artifacts.

--- scripts/gen-functionN-methods.scala
+++ scripts/gen-functionN-methods.scala
@@ -13,10 +13,10 @@ for (i <- range) {
       ") + " + a + ".hashCode"
     }
 
-  println("def valueOf[" + alphaPlusOne.toString.toUpperCase + "](" + alphaWithAnyTypes + ", fun: Int => Int)(implicit genOf" + alphaPlusOne.toString.toUpperCase + ": Generator[" + alphaPlusOne.toString.toUpperCase + "]): " + alphaPlusOne.toString.toUpperCase + " = {\n" +
+  println("def valueOf[" + alphaPlusOne.toString.toUpperCase + "](multiplier: Int, " + alphaWithAnyTypes + ")(implicit genOf" + alphaPlusOne.toString.toUpperCase + ": Generator[" + alphaPlusOne.toString.toUpperCase + "]): " + alphaPlusOne.toString.toUpperCase + " = {\n" +
           "  def combinedHashCode(" + alphaWithAnyTypes + "): Int = \n" +
           "    " + hashCodeImpl + "\n" +
-          "  val seed = (fun(combinedHashCode(" + alphaLower + "))).toLong\n" +
+          "  val seed = combinedHashCode(" + alphaLower + ").toLong * multiplier\n" +
           "  val rnd = Randomizer(seed)\n" +
           "  val (size, nextRnd) = rnd.chooseInt(1, 20)\n" +
           "  val (result, _, _) = genOf" + alphaPlusOne.toString.toUpperCase + ".next(size, Nil, nextRnd)\n" +
@@ -35,26 +35,22 @@ for (i <- range) {
   val alphaPlusOneUpper = alphaPlusOne.toString.toUpperCase
   val typeTypeOfAll = alpha.toUpperCase.map(a => "typeTagOf" + a + ": TypeTag[" + a + "]").mkString(", ") + ", " + "typeTagOf" + alphaPlusOneUpper + ": TypeTag[" + alphaPlusOneUpper + "]"
   val alphaWithTypes = alpha.map(a => a + ": " + a.toString.toUpperCase).mkString(", ")
-  val typeOfAlphaDcls = alpha.toUpperCase.map(a => "val typeOf" + a + " = typeTagOf" + a + ".tpe").mkString("\n")
+  val typeOfAlphaDcls = alpha.toUpperCase.map(a => "val typeOf" + a + " = typeTagOf" + a + ".tpe").mkString("\n          ")
   val typeOfAlphaPlusOneDcls = "val typeOf" + alphaPlusOneUpper + " = typeTagOf" + alphaPlusOneUpper + ".tpe"
   val alphaWithDollarTypeOf = alpha.map(a => a + ": $typeOf" + a.toString.toUpperCase).mkString(", ")
 
   println("implicit def function" + i + "Generator[" + alphaUpper + ", " + alphaPlusOneUpper + "](implicit genOf" + alphaPlusOneUpper + ": Generator[" + alphaPlusOneUpper + "], " + typeTypeOfAll+ "): Generator[(" + alphaUpper + ") => " + alphaPlusOneUpper + "] = {\n" +
           "  new Generator[(" + alphaUpper + ") => " + alphaPlusOneUpper + "] {\n" +
           "    def next(size: Int, edges: List[(" + alphaUpper + ") => " + alphaPlusOneUpper + "], rnd: Randomizer): ((" + alphaUpper + ") => " + alphaPlusOneUpper + ", List[(" + alphaUpper + ") => " + alphaPlusOneUpper + "], Randomizer) = {\n" +
-          "      val intToIntGen: Generator[Int => Int] = function1IntToIntGenerator\n" +
-          "      val (intToInt, _, rnd1) = intToIntGen.next(10, Nil, rnd)\n\n" +
+          "      val first1000PrimesGen: Generator[Int] = first1000Primes\n" +
+          "      val (prime, _, rnd1) = first1000PrimesGen.next(10, Nil, rnd)\n" +
+          "      val multiplier = if (prime == 2) 1 else prime\n\n" +
           "      object " + alpha.toUpperCase + "To" + alphaPlusOneUpper + " extends ((" + alphaUpper + ") => " + alphaPlusOneUpper + ") {\n" +
-          "        def apply(" + alphaWithTypes + "): " + alphaPlusOneUpper + " = org.scalatest.prop.valueOf[" + alphaPlusOneUpper + "](" + alphaLower + ", intToInt)\n" +
+          "        def apply(" + alphaWithTypes + "): " + alphaPlusOneUpper + " = org.scalatest.prop.valueOf[" + alphaPlusOneUpper + "](multiplier, " + alphaLower + ")\n" +
           "        override def toString = {\n" +
           "          " + typeOfAlphaDcls + "\n" +
           "          " + typeOfAlphaPlusOneDcls + "\n" +
-          "          val intToIntName: String = \n" +
-          "            intToInt match {\n" +
-          "              case prf: PrettyFunction1[_, _] => prf.simpleName\n" +
-          "              case _ => intToInt.toString\n" +
-          "            }\n" +
-          "          s\"(" + alphaWithDollarTypeOf + ") => org.scalatest.prop.valueOf[$typeOf" + alphaPlusOneUpper + "](" + alphaLower + ", $intToIntName)\"\n" +
+          "          s\"(" + alphaWithDollarTypeOf + ") => org.scalatest.prop.valueOf[$typeOf" + alphaPlusOneUpper + s"]($$multiplier, " + alphaLower + ")\"\n" +
           "        }\n" +
           "      }\n\n" +
           "      (" + alpha.toUpperCase + "To" + alphaPlusOneUpper + ", Nil, rnd1)\n" +
