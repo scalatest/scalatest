b50c4183fd73e5a9d201417046a22131f1189d47
Chua Chee Seng <cheeseng@amaseng.com>
2017-07-06 11:27:26 +0800

    Bumped up version number for scala xml, combinator parser and scala-js.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -23,7 +23,7 @@ object ScalatestBuild extends Build {
 
   val releaseVersion = "3.0.3"
 
-  val scalacheckVersion = "1.13.4"
+  val scalacheckVersion = "1.13.5"
 
   val githubTag = "release-3.0.3" // for scaladoc source urls
 
@@ -150,8 +150,8 @@ object ScalatestBuild extends Build {
       // if scala 2.11+ is used, add dependency on scala-xml module
       case Some((2, scalaMajor)) if scalaMajor >= 11 =>
         Seq(
-          "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
-          "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4",
+          "org.scala-lang.modules" %% "scala-xml" % "1.0.6",
+          "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.6",
           scalacheckDependency("optional")
         )
       case _ =>
@@ -188,7 +188,7 @@ object ScalatestBuild extends Build {
 
   def scalatestJSLibraryDependencies =
     Seq(
-      "org.scala-js" %% "scalajs-test-interface" % "0.6.16"
+      "org.scala-js" %% "scalajs-test-interface" % "0.6.18"
     )
 
   def scalatestTestOptions =
