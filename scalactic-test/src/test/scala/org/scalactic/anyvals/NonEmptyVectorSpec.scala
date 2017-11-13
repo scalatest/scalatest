3d0d89bfe473414eee3414c466e01508588501bc
Chua Chee Seng <cheeseng@amaseng.com>
2017-11-14 00:12:08 +0800

    Commented out par method in NumericString, and added compat import to NonEmptyXXXSpec.

--- scalactic-test/src/test/scala/org/scalactic/anyvals/NonEmptyVectorSpec.scala
+++ scalactic-test/src/test/scala/org/scalactic/anyvals/NonEmptyVectorSpec.scala
@@ -22,6 +22,10 @@ import scala.collection.mutable.ListBuffer
 import org.scalactic.{Every, One, Many, StringNormalizations}
 import org.scalactic.UnitSpec
 
+// SKIP-SCALATESTJS-START
+import org.scalatest.CompatParColls.Converters._
+// SKIP-SCALATESTJS-END
+
 class NonEmptyVectorSpec extends UnitSpec {
   "A NonEmptyVector" can "be constructed with one element" in {
     val onesie = NonEmptyVector(3)
