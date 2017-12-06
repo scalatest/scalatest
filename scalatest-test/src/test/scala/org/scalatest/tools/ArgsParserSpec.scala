9968447cad6e3cf8090e3fd7b6109508bd40877e
Chua Chee Seng <cheeseng@amaseng.com>
2017-12-06 22:39:32 +0800

    Changed -P option to accept C appended to it, and reject negative thread number passed to -P.

--- scalatest-test/src/test/scala/org/scalatest/tools/ArgsParserSpec.scala
+++ scalatest-test/src/test/scala/org/scalatest/tools/ArgsParserSpec.scala
@@ -1771,9 +1771,17 @@ class ArgsParserSpec extends FunSpec {
     assert(multiDashP.numThreads === 0)
     assert(multiDashP.enableSuiteSortingReporter === false)
 
+    intercept[IllegalArgumentException] {
+      ArgsParser.parseConcurrentConfig(List("-P0"))
+    }
+
     val singleDashPThreadNum = ArgsParser.parseConcurrentConfig(List("-P10"))
     assert(singleDashPThreadNum.numThreads === 10)
     assert(singleDashPThreadNum.enableSuiteSortingReporter === false)
+
+    intercept[IllegalArgumentException] {
+      ArgsParser.parseConcurrentConfig(List("-P-10"))
+    }
 
     val multiDashPThreadNum = ArgsParser.parseConcurrentConfig(List("-P10", "-P5"))
     assert(multiDashPThreadNum.numThreads === 10)
@@ -1790,6 +1798,10 @@ class ArgsParserSpec extends FunSpec {
     val singleDashPSThreadNum = ArgsParser.parseConcurrentConfig(List("-PS8"))
     assert(singleDashPSThreadNum.numThreads === 8)
     assert(singleDashPSThreadNum.enableSuiteSortingReporter === true)
+
+    intercept[IllegalArgumentException] {
+      ArgsParser.parseConcurrentConfig(List("-PS-8"))
+    }
 
     val multipDashPSThreadNum = ArgsParser.parseConcurrentConfig(List("-PS8", "-P10"))
     assert(multipDashPSThreadNum.numThreads === 8)
