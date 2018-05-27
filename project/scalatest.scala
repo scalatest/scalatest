91d426ec0913ba86c309cc644f877a192aa6d154
Chua Chee Seng <cheeseng@amaseng.com>
2018-05-27 11:29:37 +0800

    Re-organize implicits in Aggregating and Containing in different levels to improve compile time.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -592,7 +592,8 @@ object ScalatestBuild extends Build {
       publishLocal := {},
       sourceGenerators in Test += Def.task {
         GenAnyVals.genTest((sourceManaged in Test).value / "scala" / "org" / "scalactic" / "anyvals", version.value, scalaVersion.value)
-      }.taskValue
+      }.taskValue, 
+      scalacOptions ++= (if (scalaBinaryVersion.value == "2.10") Seq.empty else Seq("-Ypartial-unification"))
     ).dependsOn(scalactic, scalatest % "test", commonTest % "test")
 
   lazy val scalacticTestJS = Project("scalacticTestJS", file("scalactic-test.js"))
@@ -622,7 +623,8 @@ object ScalatestBuild extends Build {
       },
       publishArtifact := false,
       publish := {},
-      publishLocal := {}
+      publishLocal := {},
+      scalacOptions ++= (if (scalaBinaryVersion.value == "2.10") Seq.empty else Seq("-Ypartial-unification"))
     ).dependsOn(scalacticJS, scalatestJS % "test", commonTestJS % "test").enablePlugins(ScalaJSPlugin)
 
   lazy val scalacticTestNative = Project("scalacticTestNative", file("scalactic-test.native"))
@@ -756,7 +758,7 @@ object ScalatestBuild extends Build {
       publishArtifact := false,
       publish := {},
       publishLocal := {},
-      scalacOptions += "-Ypartial-unification"
+      scalacOptions ++= (if (scalaBinaryVersion.value == "2.10") Seq.empty else Seq("-Ypartial-unification"))
     ).dependsOn(scalatest % "test", commonTest % "test")
 
   lazy val scalatestJS = Project("scalatestJS", file("scalatest.js"))
@@ -870,6 +872,7 @@ object ScalatestBuild extends Build {
       publishArtifact := false,
       publish := {},
       publishLocal := {},
+      scalacOptions ++= (if (scalaBinaryVersion.value == "2.10") Seq.empty else Seq("-Ypartial-unification")),
       sourceGenerators in Test += {
         Def.task {
           GenScalaTestJS.genTest((sourceManaged in Test).value, version.value, scalaVersion.value)
@@ -1233,7 +1236,7 @@ object ScalatestBuild extends Build {
   def gentestsSharedSettings: Seq[Setting[_]] = Seq(
     javaHome := getJavaHome(scalaBinaryVersion.value),
     scalaVersion := buildScalaVersion,
-    scalacOptions ++= Seq("-feature", "-Ypartial-unification"),
+    scalacOptions ++= Seq("-feature") ++ (if (scalaBinaryVersion.value == "2.10") Seq.empty else Seq("-Ypartial-unification")),
     resolvers += "Sonatype Public" at "https://oss.sonatype.org/content/groups/public",
     libraryDependencies ++= crossBuildLibraryDependencies.value,
     libraryDependencies ++= gentestsLibraryDependencies,
