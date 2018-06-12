c9cd912302110a1dd3afe7d23488b9cae8ebf1ab
Chua Chee Seng <cheeseng@amaseng.com>
2018-06-12 21:21:57 +0800

    Making the generated Every to work with scala 2.13.0-M4.

--- project/GenEvery.scala
+++ project/GenEvery.scala
@@ -5,7 +5,22 @@ import scala.io.Source
 
 object GenEvery {
 
-  def transformLine(line: String): String = line
+  def transformLine(line: String, scalaVersion: String): String = {
+    if (scalaVersion startsWith "2.13")
+      line
+        .replaceAllLiterally(": Map[K, Every[T]]", ": scala.collection.MapView[K, org.scalactic.Every[T]]")
+        .replaceAllLiterally("final def to[Col[_]](implicit cbf: CanBuildFrom[Nothing, T, Col[T @uV]]): Col[T @uV] = underlying.to[Col](cbf)",
+                             "final def to[C1](factory: scala.collection.Factory[T, C1]): C1 = underlying.to(factory)")
+        .replaceAllLiterally("final def toTraversable: Traversable[T] = underlying.toTraversable", "")
+        .replaceAllLiterally("final def union[U >: T](that: GenSeq[U])(implicit cbf: CanBuildFrom[Vector[T], U, Vector[U]]): Every[U] = fromNonEmptyVector(underlying.union(that)(cbf))",
+                             """final def union[U >: T](that: Seq[U]): Every[U] = {
+                               |  val vec = underlying.union(that)
+                               |  Every(vec.head, vec.tail: _*)
+                               |}
+                             """.stripMargin)
+    else
+      line
+  }
 
   private def copyFile(sourceFile: File, destFile: File, scalaVersion: String): File = {
     destFile.getParentFile.mkdirs()
@@ -13,7 +28,7 @@ object GenEvery {
     try {
       val lines = Source.fromFile(sourceFile).getLines.toList
       for (line <- lines) {
-        destWriter.write(transformLine(line))
+        destWriter.write(transformLine(line, scalaVersion))
         destWriter.newLine()
       }
       destFile
