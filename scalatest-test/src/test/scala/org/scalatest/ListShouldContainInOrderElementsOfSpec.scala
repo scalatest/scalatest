49bd2aa28f172944487182bf6e7c436a80b57caa
Chua Chee Seng <cheeseng@amaseng.com>
2018-07-06 23:27:01 +0800

    Replaced the use of mutable.LinkedList with mutable.ListBuffer, as it is being removed in Scala 2.13.

--- scalatest-test/src/test/scala/org/scalatest/ListShouldContainInOrderElementsOfSpec.scala
+++ scalatest-test/src/test/scala/org/scalatest/ListShouldContainInOrderElementsOfSpec.scala
@@ -21,7 +21,6 @@ import org.scalactic.Prettifier
 import org.scalactic.StringNormalizations._
 import SharedHelpers._
 import FailureMessages.decorateToStringValue
-import scala.collection.mutable.LinkedList
 import exceptions.TestFailedException
 
 class ListShouldContainInOrderElementsOfSpec extends FunSpec with Matchers {
