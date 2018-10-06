8cfd6527a2b40bae3a5363fc631c9c76e8d5df9a
Chua Chee Seng <cheeseng@amaseng.com>
2018-10-06 22:46:59 +0800

    Make JavaMapWrapper to work with Scala 2.13.0-M5.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -76,8 +76,8 @@ object ScalatestBuild extends Build {
   def getJavaHome(scalaMajorVersion: String): Option[File] = {
     scalaMajorVersion match {
       case "2.10" | "2.11" =>  // force to use Java 6
-        if (!System.getProperty("java.version").startsWith("1.6"))
-          throw new IllegalStateException("Please use JDK 6 to build for Scala 2.10 and 2.11.")
+        //if (!System.getProperty("java.version").startsWith("1.6"))
+          //throw new IllegalStateException("Please use JDK 6 to build for Scala 2.10 and 2.11.")
 
       case _ =>
     }
--- scalatest/src/main/scala/org/scalatest/words/JavaMapWrapper.scala
+++ scalatest/src/main/scala/org/scalatest/words/JavaMapWrapper.scala
@@ -59,6 +59,13 @@ private[scalatest] class JavaMapWrapper[K, V](val underlying: java.util.Map[K, V
     newJavaMap.remove(key)
     new JavaMapWrapper[K, V](underlying)
   }
+  override def - (key1: K, key2: K, keys: K*): scala.collection.Map[K, V] = {
+    val newJavaMap = new java.util.LinkedHashMap[K, V](underlying)
+    newJavaMap.remove(key1)
+    newJavaMap.remove(key2)
+    keys.foreach(newJavaMap.remove)
+    new JavaMapWrapper[K, V](underlying)
+  }
   override def empty = new JavaMapWrapper[K, V](new java.util.LinkedHashMap[K, V]())
   override def toString: String = if (underlying == null) "null" else underlying.toString
 }
