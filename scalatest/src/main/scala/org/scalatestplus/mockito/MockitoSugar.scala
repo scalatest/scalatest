8baecd40878171efe9554dd18fdda6d196572e60
Chua Chee Seng <cheeseng@amaseng.com>
2018-12-26 11:16:44 +0800

    Moved classes in org.scalatest.mockito to org.scalatestplus.mockito, leaving deprecated type alias in the original package.

--- scalatest/src/main/scala/org/scalatestplus/mockito/MockitoSugar.scala
+++ scalatest/src/main/scala/org/scalatestplus/mockito/MockitoSugar.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatest.mockito
+package org.scalatestplus.mockito
 
 import org.mockito.Mockito.{mock => mockitoMock}
 import reflect.ClassTag
