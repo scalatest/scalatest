9179c057ad8c80fbbb775495688512a62887b6bd
Mark "Justin" Waks <jducoeur@artima.com>
2019-01-04 15:59:19 -0500

    First-draft documentation for the valueOf() function
    I think this is basically correct, but may wind up getting tweaked as the rest of the docs fill in. But it's a good start.

--- scalatest/src/main/scala/org/scalatest/prop/package.scala
+++ scalatest/src/main/scala/org/scalatest/prop/package.scala
@@ -20,9 +20,29 @@ import scala.annotation.tailrec
 import scala.reflect.runtime.universe.TypeTag
 
 package object prop {
-  def valueOf[A](a: Any, others: Any*)(multiplier: Int)(implicit genOfA: Generator[A]): A = {
+  /**
+    * Deterministically generate a value for the given Generator.
+    *
+    * This function takes a set of anywhere from 1-22 parameters, plus a "multiplier". It combines these to
+    * generate a pseudo-random (but deterministic) seed, feeds that into the Generator, and returns the
+    * result. Since the results are deterministic, calling this repeatedly with the same parameters will produce
+    * the same output.
+    *
+    * This is mainly helpful when generating random Functions -- since the inputs for a test run are
+    * complex, you need more than a simple random seed to reproduce the same results. In order to make
+    * this more useful, the `toString` of a instance of a Function [[Generator]] shows how to invoke
+    * `valueOf()` to reproduce the same result.
+    *
+    * @param first The first parameter to use for calculating the seed.
+    * @param others Any additional parameters to use for calculating the seed.
+    * @param multiplier A number to combine with the other parameters, to calculate the seed.
+    * @param genOfA A Generator. (Usually a Function Generator.)
+    * @tparam A The type of the Generator.
+    * @return An instance of A, computed by feeding the calculated seed into the Generator.
+    */
+  def valueOf[A](first: Any, others: Any*)(multiplier: Int)(implicit genOfA: Generator[A]): A = {
     val combinedHashCode: Int =
-      others.foldLeft(a.hashCode) { (acc, next) =>
+      others.foldLeft(first.hashCode) { (acc, next) =>
         (37 * (acc + 37)) + next.hashCode
       }
     val seed = combinedHashCode.toLong * multiplier
