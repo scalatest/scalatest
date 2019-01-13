ee34697743a91162cfa915c424fcf436ad89f6f3
bvenners <bill@artima.com>
2019-01-12 21:42:47 -0800

    Reimplemented choosePosZFiniteDouble in Randomizer.

--- scalatest/src/main/scala/org/scalatest/prop/Randomizer.scala
+++ scalatest/src/main/scala/org/scalatest/prop/Randomizer.scala
@@ -1730,25 +1730,8 @@ class Randomizer(private[scalatest] val seed: Long) { thisRandomizer =>
     * @return A value from that range, inclusive of the ends.
     */
   def choosePosZFiniteDouble(from: PosZFiniteDouble, to: PosZFiniteDouble): (PosZFiniteDouble, Randomizer) = {
-// XXX
-    if (from == to) {
-      (from, thisRandomizer)
-    }
-    else {
-      val min = math.min(from, to)
-      val max = math.max(from, to)
-
-      val nextPair = nextPosZFiniteDouble
-      val (nextValue, nextRnd) = nextPair
-
-      if (nextValue >= min && nextValue <= max)
-        nextPair
-      else {
-        val (between0And1, nextNextRnd) = nextRnd.nextDoubleBetween0And1
-        val nextBetween = min + (between0And1 * (max - min)).abs
-        (PosZFiniteDouble.ensuringValid(nextBetween), nextRnd)
-      }
-    }
+    val (n, nextRnd) = choosePositiveOrZeroDouble(from.value, to.value)
+    (PosZFiniteDouble.ensuringValid(n), nextRnd)
   }
 
   /**
