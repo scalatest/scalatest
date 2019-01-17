059036d22b748d6eb0f7ec6e166afd41d36f1f02
Woodz <richard.wise@redmart.com>
2019-01-17 18:29:00 +0800

    Move `PatienceConfig` out of `AbstractPatienceConfiguration` Change `PatienceConfig` to no longer be a path-dependant type to avoid  confusion with multiple instances. Add a protected `defaultPatienceConfig` to  `AbstractPatienceConfiguration` to allow scaling and overriding by  derived traits Resolve: #1487

--- scalatest/src/main/scala/org/scalatest/concurrent/AbstractPatienceConfiguration.scala
+++ scalatest/src/main/scala/org/scalatest/concurrent/AbstractPatienceConfiguration.scala
@@ -53,42 +53,8 @@ import org.scalatest.time.{Span, Millis}
  */
 trait AbstractPatienceConfiguration extends ScaledTimeSpans {
 
-  /**
-   * Configuration object for asynchronous constructs, such as those provided by traits <a href="Eventually.html"><code>Eventually</code></a> and
-   * <a href="AsyncAssertions.html"><code>AsyncAssertions</code></a>.
-   *
-   * <p>
-   * The default values for the parameters are:
-   * </p>
-   *
-   * <table style="border-collapse: collapse; border: 1px solid black">
- * <tr><th style="background-color: #CCCCCC; border-width: 1px; padding: 3px; text-align: center; border: 1px solid black"><strong>Configuration Parameter</strong></th><th style="background-color: #CCCCCC; border-width: 1px; padding: 3px; text-align: center; border: 1px solid black"><strong>Default Value</strong></th></tr>
-   * <tr>
-   * <td style="border-width: 1px; padding: 3px; border: 1px solid black; text-align: center">
-   * <code>timeout</code>
-   * </td>
-   * <td style="border-width: 1px; padding: 3px; border: 1px solid black; text-align: center">
-   * <code>scaled(150 milliseconds)</code>
-   * </td>
-   * </tr>
-   * <tr>
-   * <td style="border-width: 1px; padding: 3px; border: 1px solid black; text-align: center">
-   * <code>interval</code>
-   * </td>
-   * <td style="border-width: 1px; padding: 3px; border: 1px solid black; text-align: center">
-   * <code>scaled(15 milliseconds)</code>
-   * </td>
-   * </tr>
-   * </table>
-   *
-   * @param timeout the maximum amount of time to wait for an asynchronous operation to complete before giving up and throwing
-   *   <code>TestFailedException</code>.
-   * @param interval the amount of time to sleep between each check of the status of an asynchronous operation when polling
-   *
-   * @author Bill Venners
-   * @author Chua Chee Seng
-   */
-  final case class PatienceConfig(timeout: Span = scaled(Span(150, Millis)), interval: Span = scaled(Span(15, Millis)))
+  protected def defaultPatienceConfig: PatienceConfig =
+    PatienceConfig(timeout = scaled(Span(150, Millis)), interval = scaled(Span(15, Millis)))
 
   /**
    * Returns a <code>PatienceConfig</code> value providing default configuration values if implemented and made implicit in subtraits.
