7b913eb5e97e99362fc82f5f69bb1b9a88b1d949
Chua Chee Seng <cheeseng@amaseng.com>
2019-03-25 11:41:08 +0800

    Fixed compile error when compiling under Scala 2.10.

--- scalatest/src/main/scala/org/scalatest/freespec/AnyFreeSpecLike.scala
+++ scalatest/src/main/scala/org/scalatest/freespec/AnyFreeSpecLike.scala
@@ -15,7 +15,7 @@
  */
 package org.scalatest.freespec
 
-import org.scalactic.{FailureMessages => _, UnquotedString => _, _}
+import org.scalactic.{Resources => _, FailureMessages => _, UnquotedString => _, _}
 import org.scalatest._
 import org.scalatest.Suite.autoTagClassAnnotations
 import org.scalatest.words.BehaveWord
