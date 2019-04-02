733f5d41c0fca1aaea64ffc6b4225e3ae4180d82
Chua Chee Seng <cheeseng@amaseng.com>
2019-04-02 23:12:34 +0800

    Reduced test failures in ShouldContainElementSpec when built under Scala 2.13.0-M5.

--- scalatest-test/src/test/scala/org/scalatest/ShouldContainElementSpec.scala
+++ scalatest-test/src/test/scala/org/scalatest/ShouldContainElementSpec.scala
@@ -616,7 +616,7 @@ class ShouldContainElementSpec extends FunSpec with Checkers with ReturnsNormall
         val caught = intercept[TestFailedException] {
           HashSet(1, 2) should contain (3)
         }
-        assert(caught.getMessage === "Set(1, 2) did not contain element 3")
+        assert(caught.getMessage === HashSet(1, 2) + " did not contain element 3")
       }
 
       it("should throw TestFailedException if set contains the specified element, when used with not") {
@@ -624,17 +624,17 @@ class ShouldContainElementSpec extends FunSpec with Checkers with ReturnsNormall
         val caught1 = intercept[TestFailedException] {
           HashSet(1, 2) should not contain (2)
         }
-        assert(caught1.getMessage === "Set(1, 2) contained element 2")
+        assert(caught1.getMessage === HashSet(1, 2) + " contained element 2")
 
         val caught2 = intercept[TestFailedException] {
           HashSet(1, 2) should not (contain (2))
         }
-        assert(caught2.getMessage === "Set(1, 2) contained element 2")
+        assert(caught2.getMessage === HashSet(1, 2) + " contained element 2")
 
         val caught3 = intercept[TestFailedException] {
           HashSet(1, 2) should (not contain (2))
         }
-        assert(caught3.getMessage === "Set(1, 2) contained element 2")
+        assert(caught3.getMessage === HashSet(1, 2) + " contained element 2")
       }
 
       it("should throw a TestFailedException when set doesn't contain the specified element and used in a logical-and expression") {
@@ -642,12 +642,12 @@ class ShouldContainElementSpec extends FunSpec with Checkers with ReturnsNormall
         val caught1 = intercept[TestFailedException] {
           HashSet(1, 2) should { contain (5) and (contain (2 - 1)) }
         }
-        assert(caught1.getMessage === "Set(1, 2) did not contain element 5")
+        assert(caught1.getMessage === HashSet(1, 2) + " did not contain element 5")
 
         val caught2 = intercept[TestFailedException] {
           HashSet(1, 2) should (contain (5) and contain (2 - 1))
         }
-        assert(caught2.getMessage === "Set(1, 2) did not contain element 5")
+        assert(caught2.getMessage === HashSet(1, 2) + " did not contain element 5")
       }
 
       it("should throw a TestFailedException when set doesn't contain the specified element and used in a logical-or expression") {
@@ -655,12 +655,12 @@ class ShouldContainElementSpec extends FunSpec with Checkers with ReturnsNormall
         val caught1 = intercept[TestFailedException] {
           HashSet(1, 2) should { contain (55) or (contain (22)) }
         }
-        assert(caught1.getMessage === "Set(1, 2) did not contain element 55, and Set(1, 2) did not contain element 22")
+        assert(caught1.getMessage === HashSet(1, 2) + " did not contain element 55, and " + HashSet(1, 2) + " did not contain element 22")
 
         val caught2 = intercept[TestFailedException] {
           HashSet(1, 2) should (contain (55) or contain (22))
         }
-        assert(caught2.getMessage === "Set(1, 2) did not contain element 55, and Set(1, 2) did not contain element 22")
+        assert(caught2.getMessage === HashSet(1, 2) + " did not contain element 55, and " + HashSet(1, 2) + " did not contain element 22")
       }
 
       it("should throw a TestFailedException when set contains the specified element and used in a logical-and expression with not") {
@@ -668,17 +668,17 @@ class ShouldContainElementSpec extends FunSpec with Checkers with ReturnsNormall
         val caught1 = intercept[TestFailedException] {
           HashSet(1, 2) should { not { contain (3) } and not { contain (2) }}
         }
-        assert(caught1.getMessage === "Set(1, 2) did not contain element 3, but Set(1, 2) contained element 2")
+        assert(caught1.getMessage === HashSet(1, 2) + " did not contain element 3, but " + HashSet(1, 2) + " contained element 2")
 
         val caught2 = intercept[TestFailedException] {
           HashSet(1, 2) should ((not contain (3)) and (not contain (2)))
         }
-        assert(caught2.getMessage === "Set(1, 2) did not contain element 3, but Set(1, 2) contained element 2")
+        assert(caught2.getMessage === HashSet(1, 2) + " did not contain element 3, but " + HashSet(1, 2) + " contained element 2")
 
         val caught3 = intercept[TestFailedException] {
           HashSet(1, 2) should (not contain (3) and not contain (2))
         }
-        assert(caught3.getMessage === "Set(1, 2) did not contain element 3, but Set(1, 2) contained element 2")
+        assert(caught3.getMessage === HashSet(1, 2) + " did not contain element 3, but " + HashSet(1, 2) + " contained element 2")
       }
 
       it("should throw a TestFailedException when set contains the specified element and used in a logical-or expression with not") {
@@ -686,17 +686,17 @@ class ShouldContainElementSpec extends FunSpec with Checkers with ReturnsNormall
         val caught1 = intercept[TestFailedException] {
           HashSet(1, 2) should { not { contain (2) } or not { contain (2) }}
         }
-        assert(caught1.getMessage === "Set(1, 2) contained element 2, and Set(1, 2) contained element 2")
+        assert(caught1.getMessage === HashSet(1, 2) + " contained element 2, and " + HashSet(1, 2) + " contained element 2")
 
         val caught2 = intercept[TestFailedException] {
           HashSet(1, 2) should ((not contain (2)) or (not contain (2)))
         }
-        assert(caught2.getMessage === "Set(1, 2) contained element 2, and Set(1, 2) contained element 2")
+        assert(caught2.getMessage === HashSet(1, 2) + " contained element 2, and " + HashSet(1, 2) + " contained element 2")
 
         val caught3 = intercept[TestFailedException] {
           HashSet(1, 2) should (not contain (2) or not contain (2))
         }
-        assert(caught3.getMessage === "Set(1, 2) contained element 2, and Set(1, 2) contained element 2")
+        assert(caught3.getMessage === HashSet(1, 2) + " contained element 2, and " + HashSet(1, 2) + " contained element 2")
       }
 
       // SKIP-SCALATESTJS-START
