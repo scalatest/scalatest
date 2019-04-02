0f9eac2025a7fd88488def4ca8948f569be01176
Chua Chee Seng <cheeseng@amaseng.com>
2019-04-02 11:29:41 +0800

    Use fixed 0.14.0-bin-20190331-c059b64-NIGHTLY dotty version, since the latest build breaks our build.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -2134,7 +2134,8 @@ object ScalatestBuild {
 
   import dotty.tools.sbtplugin.DottyPlugin.autoImport._
   // List of available night build at https://repo1.maven.org/maven2/ch/epfl/lamp/dotty-compiler_0.13/
-  lazy val dottyVersion = dottyLatestNightlyBuild.get
+  //lazy val dottyVersion = dottyLatestNightlyBuild.get
+  lazy val dottyVersion = "0.14.0-bin-20190331-c059b64-NIGHTLY"
   lazy val dottySettings = List(
     scalaVersion := dottyVersion,
     libraryDependencies := libraryDependencies.value.map(_.withDottyCompat(scalaVersion.value)),
