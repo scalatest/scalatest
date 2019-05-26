8659c20e3177810d470527fc4484c3369a5613c2
Chua Chee Seng <cheeseng@amaseng.com>
2019-05-26 15:35:14 +0800

    Updated for Scala 2.13.0-RC2 and 3.0.8-RC4 release.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -25,7 +25,7 @@ object ScalatestBuild extends Build {
   // > ++ 2.10.5
   val buildScalaVersion = "2.12.8"
 
-  val releaseVersion = "3.0.8-RC3"
+  val releaseVersion = "3.0.8-RC4"
 
   val previousReleaseVersion = "3.0.5"
 
