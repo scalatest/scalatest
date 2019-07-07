7b389ba771ea5b60997dd049dca456f77ca73dbb
bvenners <bill@artima.com>
2019-07-06 22:46:22 -0400

    Fixed bug causing "is pending" (as opposed to "in pending") tests in AsyncWordSpec to succeed rather than be marked as pending.

--- scalatest/src/main/scala/org/scalatest/wordspec/AsyncWordSpecLike.scala
+++ scalatest/src/main/scala/org/scalatest/wordspec/AsyncWordSpecLike.scala
@@ -47,7 +47,7 @@ trait AsyncWordSpecLike extends AsyncTestSuite with AsyncTestRegistration with S
   private[scalatest] def transformPendingToOutcome(testFun: () => PendingStatement): () => AsyncTestHolder =
     () => {
       PastAsyncTestHolder(
-        try { testFun; Succeeded } // testFun TODO: Add ()'s to testFun after writing a failing test.
+        try { testFun(); Succeeded }
         catch {
           case ex: TestCanceledException => Canceled(ex)
           case _: TestPendingException => Pending
