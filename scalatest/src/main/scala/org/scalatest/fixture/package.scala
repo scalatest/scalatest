5162f085f26e07a726d1e4d825021c07319b7eb0
Chua Chee Seng <cheeseng@amaseng.com>
2019-07-17 22:58:25 +0800

    Enhanced deprecation messages in org.scalatest.fixture.package.scala.

--- scalatest/src/main/scala/org/scalatest/fixture/package.scala
+++ scalatest/src/main/scala/org/scalatest/fixture/package.scala
@@ -20,93 +20,93 @@ package org.scalatest
  */
 package object fixture {
 
-  @deprecated("Please use org.scalatest.funsuite.FixtureAnyFunSuiteLike instead")
+  @deprecated("The org.scalatest.fixture.FunSuiteLike trait has been moved and renamed. Please use org.scalatest.funsuite.FixtureAnyFunSuiteLike instead.")
   type FunSuiteLike = org.scalatest.funsuite.FixtureAnyFunSuiteLike
 
-  @deprecated("Please use org.scalatest.funsuite.FixtureAnyFunSuite instead")
+  @deprecated("The org.scalatest.fixture.FunSuite trait has been moved and renamed. Please use org.scalatest.funsuite.FixtureAnyFunSuite instead.")
   type FunSuite = org.scalatest.funsuite.FixtureAnyFunSuite
 
   // SKIP-SCALATESTNATIVE-START
-  @deprecated("Please use org.scalatest.funsuite.FixtureAsyncFunSuiteLike instead")
+  @deprecated("The org.scalatest.fixture.AsyncFunSuiteLike trait has been moved and renamed. Please use org.scalatest.funsuite.FixtureAsyncFunSuiteLike instead.")
   type AsyncFunSuiteLike = org.scalatest.funsuite.FixtureAsyncFunSuiteLike
 
-  @deprecated("Please use org.scalatest.funsuite.FixtureAsyncFunSuite instead")
+  @deprecated("The org.scalatest.fixture.AsyncFunSuite trait has been moved and renamed. Please use org.scalatest.funsuite.FixtureAsyncFunSuite instead.")
   type AsyncFunSuite = org.scalatest.funsuite.FixtureAsyncFunSuite
   // SKIP-SCALATESTNATIVE-END
 
-  @deprecated("Please use org.scalatest.featurespec.FixtureAnyFeatureSpecLike instead")
+  @deprecated("The org.scalatest.fixture.FeatureSpecLike trait has been moved and renamed. Please use org.scalatest.featurespec.FixtureAnyFeatureSpecLike instead.")
   type FeatureSpecLike = org.scalatest.featurespec.FixtureAnyFeatureSpecLike
 
-  @deprecated("Please use org.scalatest.featurespec.FixtureAnyFeatureSpec instead")
+  @deprecated("The org.scalatest.fixture.FeatureSpec trait has been moved and renamed. Please use org.scalatest.featurespec.FixtureAnyFeatureSpec instead.")
   type FeatureSpec = org.scalatest.featurespec.FixtureAnyFeatureSpec
 
   // SKIP-SCALATESTNATIVE-START
-  @deprecated("Please use org.scalatest.featurespec.FixtureAsyncFeatureSpecLike instead")
+  @deprecated("The org.scalatest.fixture.AsyncFeatureSpecLike trait has been moved and renamed. Please use org.scalatest.featurespec.FixtureAsyncFeatureSpecLike instead.")
   type AsyncFeatureSpecLike = org.scalatest.featurespec.FixtureAsyncFeatureSpecLike
 
-  @deprecated("Please use org.scalatest.featurespec.FixtureAsyncFeatureSpec instead")
+  @deprecated("The org.scalatest.fixture.AsyncFeatureSpec trait has been moved and renamed. Please use org.scalatest.featurespec.FixtureAsyncFeatureSpec instead.")
   type AsyncFeatureSpec = org.scalatest.featurespec.FixtureAsyncFeatureSpec
   // SKIP-SCALATESTNATIVE-END
   
-  @deprecated("Please use org.scalatest.funsuite.FixtureAnyFunSpec instead")
+  @deprecated("The org.scalatest.fixture.FunSpecLike trait has been moved and renamed. Please use org.scalatest.funspec.FixtureAnyFunSpecLike instead.")
   type FunSpecLike = org.scalatest.funspec.FixtureAnyFunSpecLike
 
-  @deprecated("Please use org.scalatest.funsuite.FixtureAnyFunSpecLike instead")
+  @deprecated("The org.scalatest.fixture.FunSpec trait has been moved and renamed. Please use org.scalatest.funspec.FixtureAnyFunSpec instead.")
   type FunSpec = org.scalatest.funspec.FixtureAnyFunSpec
 
   // SKIP-SCALATESTNATIVE-START
-  @deprecated("Please use org.scalatest.funspec.FixtureAsyncFunSpecLike instead")
+  @deprecated("The org.scalatest.fixture.AsyncFunSpecLike trait has been moved and renamed. Please use org.scalatest.funspec.FixtureAsyncFunSpecLike instead.")
   type AsyncFunSpecLike = org.scalatest.funspec.FixtureAsyncFunSpecLike
 
-  @deprecated("Please use org.scalatest.funspec.FixtureAsyncFunSpec instead")
+  @deprecated("The org.scalatest.fixture.AsyncFunSpec trait has been moved and renamed. Please use org.scalatest.funspec.FixtureAsyncFunSpec instead.")
   type AsyncFunSpec = org.scalatest.funspec.FixtureAsyncFunSpec
   // SKIP-SCALATESTNATIVE-END
 
-  @deprecated("Please use org.scalatest.freespec.FixtureAnyFreeSpecLike instead")
+  @deprecated("The org.scalatest.fixture.FreeSpecLike trait has been moved and renamed. Please use org.scalatest.freespec.FixtureAnyFreeSpecLike instead.")
   type FreeSpecLike = org.scalatest.freespec.FixtureAnyFreeSpecLike
 
-  @deprecated("Please use org.scalatest.freespec.FixtureAnyFreeSpec instead")
+  @deprecated("The org.scalatest.fixture.FreeSpec trait has been moved and renamed. Please use org.scalatest.freespec.FixtureAnyFreeSpec instead.")
   type FreeSpec = org.scalatest.freespec.FixtureAnyFreeSpec
 
   // SKIP-SCALATESTNATIVE-START
-  @deprecated("Please use org.scalatest.freespec.FixtureAsyncFreeSpecLike instead")
+  @deprecated("The org.scalatest.fixture.AsyncFreeSpecLike trait has been moved and renamed. Please use org.scalatest.freespec.FixtureAsyncFreeSpecLike instead.")
   type AsyncFreeSpecLike = org.scalatest.freespec.FixtureAsyncFreeSpecLike
 
-  @deprecated("Please use org.scalatest.freespec.FixtureAsyncFreeSpec instead")
+  @deprecated("The org.scalatest.fixture.AsyncFreeSpec trait has been moved and renamed. Please use org.scalatest.freespec.FixtureAsyncFreeSpec instead.")
   type AsyncFreeSpec = org.scalatest.freespec.FixtureAsyncFreeSpec
   // SKIP-SCALATESTNATIVE-END
 
-  @deprecated("Please use org.scalatest.flatspec.FixtureAnyFlatSpecLike instead")
+  @deprecated("The org.scalatest.fixture.FlatSpecLike trait has been moved and renamed. Please use org.scalatest.flatspec.FixtureAnyFlatSpecLike instead.")
   type FlatSpecLike = org.scalatest.flatspec.FixtureAnyFlatSpecLike
 
-  @deprecated("Please use org.scalatest.flatspec.FixtureAnyFlatSpec instead")
+  @deprecated("The org.scalatest.fixture.FlatSpec trait has been moved and renamed. Please use org.scalatest.flatspec.FixtureAnyFlatSpec instead.")
   type FlatSpec = org.scalatest.flatspec.FixtureAnyFlatSpec
 
   // SKIP-SCALATESTNATIVE-START
-  @deprecated("Please use org.scalatest.flatspec.FixtureAsyncFlatSpecLike instead")
+  @deprecated("The org.scalatest.fixture.AsyncFlatSpecLike trait has been moved and renamed. Please use org.scalatest.flatspec.FixtureAsyncFlatSpecLike instead.")
   type AsyncFlatSpecLike = org.scalatest.flatspec.FixtureAsyncFlatSpecLike
 
-  @deprecated("Please use org.scalatest.flatspec.FixtureAsyncFlatSpec instead")
+  @deprecated("The org.scalatest.fixture.AsyncFlatSpec trait has been moved and renamed. Please use org.scalatest.flatspec.FixtureAsyncFlatSpec instead.")
   type AsyncFlatSpec = org.scalatest.flatspec.FixtureAsyncFlatSpec
   // SKIP-SCALATESTNATIVE-END
 
-  @deprecated("Please use org.scalatest.propspec.FixtureAnyPropSpecLike instead")
+  @deprecated("The org.scalatest.fixture.PropSpecLike trait has been moved and renamed. Please use org.scalatest.flatspec.FixtureAnyPropSpecLike instead.")
   type PropSpecLike = org.scalatest.propspec.FixtureAnyPropSpecLike
 
-  @deprecated("Please use org.scalatest.propspec.FixtureAnyPropSpec instead")
+  @deprecated("The org.scalatest.fixture.PropSpec trait has been moved and renamed. Please use org.scalatest.flatspec.FixtureAnyPropSpec instead.")
   type PropSpec = org.scalatest.propspec.FixtureAnyPropSpec
 
-  @deprecated("Please use org.scalatest.wordspec.FixtureAnyWordSpec instead")
+  @deprecated("The org.scalatest.fixture.WordSpec trait has been moved and renamed. Please use org.scalatest.wordspec.FixtureAnyWordSpec instead.")
   type WordSpec = org.scalatest.wordspec.FixtureAnyWordSpec
 
-  @deprecated("Please use org.scalatest.wordspec.FixtureAnyWordSpecLike instead")
+  @deprecated("The org.scalatest.fixture.WordSpecLike trait has been moved and renamed. Please use org.scalatest.wordspec.FixtureAnyWordSpecLike instead.")
   type WordSpecLike = org.scalatest.wordspec.FixtureAnyWordSpecLike
 
   // SKIP-SCALATESTNATIVE-START
-  @deprecated("Please use org.scalatest.wordspec.FixtureAsyncWordSpecLike instead")
+  @deprecated("The org.scalatest.fixture.AsyncWordSpecLike trait has been moved and renamed. Please use org.scalatest.wordspec.FixtureAsyncWordSpecLike instead.")
   type AsyncWordSpecLike = org.scalatest.wordspec.FixtureAsyncWordSpecLike
 
-  @deprecated("Please use org.scalatest.wordspec.FixtureAsyncWordSpec instead")
+  @deprecated("The org.scalatest.fixture.AsyncWordSpec trait has been moved and renamed. Please use org.scalatest.wordspec.FixtureAsyncWordSpec instead.")
   type AsyncWordSpec = org.scalatest.wordspec.FixtureAsyncWordSpec
   // SKIP-SCALATESTNATIVE-END
 }
