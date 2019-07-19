728f76efbb08cd0fa3622d47a670d54fe3e56777
bvenners <bill@artima.com>
2019-07-19 12:26:43 -0400

    Moved and renamed org.scalatest.DiagrammedAssertions to org.scalatest.diagrams.Diagrams for future modularization.

--- scalatest/src/main/scala/org/scalatest/package.scala
+++ scalatest/src/main/scala/org/scalatest/package.scala
@@ -330,4 +330,22 @@ package object scalatest {
   @deprecated("The org.scalatest.AsyncWordSpec trait has been moved and renamed. Please use org.scalatest.wordspec.AsyncWordSpec instead.")
   type AsyncWordSpec = wordspec.AsyncWordSpec
   // SKIP-SCALATESTNATIVE-END
+
+  /**
+   * <strong>The name <code>org.scalatest.DiagrammedAssertions</code> has been deprecated and will be removed in a future version of ScalaTest. Please use
+   * its new name, <code>org.scalatest.diagrams.Diagrams</code>, instead.</strong>
+   *
+   * <p>This type has been renamed to suport future modularization of ScalaTest.</p>
+   */
+  @deprecated("The org.scalatest.DiagrammedAssertions trait has been moved and renamed. Please use org.scalatest.diagrams.Diagrams instead.")
+  type DiagrammedAssertions = diagrams.Diagrams
+
+  /**
+   * <strong>The name <code>org.scalatest.DiagrammedAssertions</code> has been deprecated and will be removed in a future version of ScalaTest. Please use
+   * its new name, <code>org.scalatest.diagrams.Diagrams</code>, instead.</strong>
+   *
+   * <p>This type has been renamed to suport future modularization of ScalaTest.</p>
+   */
+  @deprecated("The org.scalatest.DiagrammedAssertions object has been moved and renamed. Please use org.scalatest.diagrams.Diagrams instead.")
+  val DiagrammedAssertions = diagrams.Diagrams
 }
