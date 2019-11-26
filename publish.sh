0df2f5971418574c55176ff1ba4c033123c60887
Chua Chee Seng <cheeseng@amaseng.com>
2019-11-26 21:53:26 +0800

    Adjustments for 3.1.0 final release.

--- publish.sh
+++ publish.sh
@@ -1,9 +1,11 @@
 sbt scalatestCompatible/clean scalatestCompatible/publishSigned
-export SCALAJS_VERSION=0.6.29
+export SCALAJS_VERSION=0.6.31
 sbt "project scalatestApp" clean +publishSigned 
 sbt "project scalatestAppJS" clean +publishSigned
 export SCALAJS_VERSION=1.0.0-M8
-sbt "project scalatestAppJS" clean +publishSigned
+sbt ++2.11.12 "project scalatestAppJS" clean publishSigned
+sbt ++2.12.10 "project scalatestAppJS" clean publishSigned
+sbt ++2.13.1 "project scalatestAppJS" clean publishSigned
 sbt ++2.11.12 "project scalatestAppNative" clean publishSigned
 sbt scalacticDotty/clean scalacticDotty/publishSigned
 sbt scalatestDotty/clean scalatestDotty/publishSigned
