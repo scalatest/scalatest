31c7a329bae61456dba9334e852bbfa07ee52a36
Chua Chee Seng <cheeseng@amaseng.com>
2020-03-05 22:17:52 +0800

    Updated publish.sh and bumped up release version to 3.2.0-M4.

--- project/BuildCommons.scala
+++ project/BuildCommons.scala
@@ -7,7 +7,7 @@ trait BuildCommons {
 
   lazy val supportedScalaVersions = List("2.13.1", "2.12.10", "2.11.12", "2.10.7")
 
-  val releaseVersion = "3.2.0-M3"
+  val releaseVersion = "3.2.0-M4"
 
   val previousReleaseVersion = "3.1.1"
 
