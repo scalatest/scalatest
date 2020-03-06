36b54c21ad31099ae816271148ae7a43f44e5568
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-03-06 07:28:03 +0100

    Drop tasty reflect given imports

--- dotty/common-test/src/main/scala/org/scalatest/LineNumberHelper.scala
+++ dotty/common-test/src/main/scala/org/scalatest/LineNumberHelper.scala
@@ -9,7 +9,7 @@ private[scalatest] trait LineNumberHelper {
 
 object LineNumberMacro {
   def thisLineNumberImpl(implicit qctx: QuoteContext): Expr[Int] = {
-    import qctx.tasty.{_, given _}
+    import qctx.tasty._
     Expr(rootPosition.startLine)
   }
 }
