f69b1dad8a8574f0be6823ca61b911b33d368db6
Arnout Engelen <arnout@bzzt.net>
2020-03-16 17:24:46 +0100

    Avoid polling Scala futures
    Futures contained a futureValueImpl which would poll the future.
    
    Futures is implemented by ScalaFutures and JavaFutures, where JavaFutures had an
    override for this method to avoid the polling.
    
    This PR moves the futureValueImpl implementation to ScalaFutures (since it was only
    used there anyway), and removes the need to poll.
    
    This also removes (the need for) FuturesSpec, since that mainly tested the polling
    mechanism that is now neither used in the Java implementation nor in the Scala one.
    The actual logic is tested in JavaFuturesSpec and ScalaFuturesSpec respectively.

--- jvm/scalatest-test/src/test/scala/org/scalatest/concurrent/ScalaFuturesSpec.scala
+++ jvm/scalatest-test/src/test/scala/org/scalatest/concurrent/ScalaFuturesSpec.scala
@@ -177,7 +177,7 @@ class ScalaFuturesSpec extends AnyFunSpec with Matchers with OptionValues with S
           neverReadyCountingFuture.futureValue
         }
 
-        caught.message.value should be (Resources.wasNeverReady(count.toString, "15 milliseconds"))
+        caught.message.value should be (Resources.wasNeverReady("150 milliseconds"))
         caught.failedCodeLineNumber.value should equal (thisLineNumber - 4)
         caught.failedCodeFileName.value should be ("ScalaFuturesSpec.scala")
       }
@@ -270,26 +270,14 @@ class ScalaFuturesSpec extends AnyFunSpec with Matchers with OptionValues with S
       // SKIP-SCALATESTJS,NATIVE-END
 
       it("should allow TestPendingException, which does not normally cause a test to fail, through immediately when thrown") {
-        val tpeFuture =
-          new FutureConcept[String] {
-            def eitherValue: Option[Either[Throwable, String]] = Some(Left(new TestPendingException))
-            def isExpired: Boolean = false
-            def isCanceled: Boolean = false
-            def awaitAtMost(span: Span): String = throw new TestPendingException
-          }
+        val tpeFuture = FutureOfScala.failed(new TestPendingException)  
         intercept[TestPendingException] {
           tpeFuture.futureValue
         }
       }
 
       it("should allow TestCanceledException, which does not normally cause a test to fail, through immediately when thrown") {
-        val tpeFuture =
-          new FutureConcept[String] {
-            def eitherValue: Option[Either[Throwable, String]] = Some(Left(new TestCanceledException(0)))
-            def isExpired: Boolean = false
-            def isCanceled: Boolean = false
-            def awaitAtMost(span: Span): String = throw new TestCanceledException(0)
-          }
+        val tpeFuture = FutureOfScala.failed(new TestCanceledException(0))  
         intercept[TestCanceledException] {
           tpeFuture.futureValue
         }
@@ -340,50 +328,6 @@ class ScalaFuturesSpec extends AnyFunSpec with Matchers with OptionValues with S
         caught.getMessage should be ("oops")
       }
 
-      it("should query the future just once if the future is ready the first time") {
-        var count = 0
-        val countingFuture = newAlreadySucceededCountingFuture { count += 1 }
-        whenReady(countingFuture) { s =>
-          s should equal ("hi")
-        }
-        count should equal (1)
-      }
-
-      it("should query the future five times if the future is not ready four times before finally being ready the fifth time") {
-        var count = 0
-        val countingFuture =
-          new FutureOfScala[String] {
-            var gotToFive = false
-            // These 2 ??? is needed to compile in 2.12, not really used though.
-            def transform[S](f: Try[String] => Try[S])(implicit executor: ExecutionContext): FutureOfScala[S] = ???
-            def transformWith[S](f: Try[String] => FutureOfScala[S])(implicit executor: ExecutionContext): FutureOfScala[S] = ???
-            def isCompleted = gotToFive
-            def onComplete[U](func: Try[String] => U)(implicit executor: ExecutionContext): Unit = {}
-            def value: Option[Try[String]] = {
-              count += 1
-              if (count < 5) None 
-              else {
-                gotToFive = true
-                Some(Success("hi"))
-              }
-            }
-
-            @throws(classOf[Exception])
-            def result(atMost: Duration)(implicit permit: CanAwait): String = neverReadyFuture.result(atMost)
-
-            @throws(classOf[TimeoutException])
-            @throws(classOf[InterruptedException])
-            def ready(atMost: Duration)(implicit permit: CanAwait): this.type = {
-              neverReadyFuture.ready(atMost)
-              this
-            }
-          }
-        whenReady(countingFuture) { s =>
-          s should equal ("hi")
-        }
-        count should equal (5)
-      }
-
       it("should eventually blow up with a TFE if the future is never ready") {
 
         var count = 0
@@ -394,7 +338,7 @@ class ScalaFuturesSpec extends AnyFunSpec with Matchers with OptionValues with S
           }
         }
 
-        caught.message.value should be (Resources.wasNeverReady(count.toString, "15 milliseconds"))
+        caught.message.value should be (Resources.wasNeverReady("150 milliseconds"))
         caught.failedCodeLineNumber.value should equal (thisLineNumber - 6)
         caught.failedCodeFileName.value should be ("ScalaFuturesSpec.scala")
       }
@@ -500,13 +444,9 @@ class ScalaFuturesSpec extends AnyFunSpec with Matchers with OptionValues with S
 
       // Same thing here and in 2.0 need to add a test for TestCanceledException
       it("should allow TestPendingException, which does not normally cause a test to fail, through immediately when thrown") {
-        val tpeFuture =
-          new FutureConcept[String] {
-            def eitherValue: Option[Either[Throwable, String]] = Some(Left(new TestPendingException))
-            def isExpired: Boolean = false
-            def isCanceled: Boolean = false
-            def awaitAtMost(span: Span): String = throw new TestPendingException
-          }
+        val promise = Promise[String]
+        promise.failure(new TestPendingException)
+        val tpeFuture = promise.future  
         intercept[TestPendingException] {
           whenReady(tpeFuture) { s =>
             s should equal ("hi")
@@ -514,13 +454,9 @@ class ScalaFuturesSpec extends AnyFunSpec with Matchers with OptionValues with S
         }
       }
       it("should allow TestCanceledException, which does not normally cause a test to fail, through immediately when thrown") {
-        val tpeFuture =
-          new FutureConcept[String] {
-            def eitherValue: Option[Either[Throwable, String]] = Some(Left(new TestCanceledException(0)))
-            def isExpired: Boolean = false
-            def isCanceled: Boolean = false
-            def awaitAtMost(span: Span): String = throw new TestCanceledException(0)
-          }
+        val promise = Promise[String]
+        promise.failure(new TestCanceledException(0))
+        val tpeFuture = promise.future  
         intercept[TestCanceledException] {
           whenReady(tpeFuture) { s =>
             s should equal ("hi")
