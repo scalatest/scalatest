4b65d1728b18f093f374ae7b597eec22115af519
Bùi Việt Thành <thanhbv@sandinh.net>
2020-03-23 11:53:50 +0700

    Fix an issue caused by lampepfl/dotty#8581

--- dotty/scalactic/src/main/scala/org/scalactic/source/Position.scala
+++ dotty/scalactic/src/main/scala/org/scalactic/source/Position.scala
@@ -69,10 +69,12 @@ object Position {
   private def genPosition(implicit qctx: QuoteContext): Expr[Position] = {
     import qctx.tasty._
 
-    val file = rootPosition.sourceFile
+    val pos = rootPosition
+    val file = pos.sourceFile
     val fileName: String = file.jpath.getFileName.toString
     val filePath: String = if (showScalacticFillFilePathnames) file.toString else Resources.pleaseDefineScalacticFillFilePathnameEnvVar
-    val lineNo: Int = rootPosition.startLine
+    // Need check `pos.exists` here because https://github.com/lampepfl/dotty/issues/8581
+    val lineNo: Int = if (pos.exists) pos.startLine else -1
     '{ Position(${Expr(fileName)}, ${Expr(filePath)}, ${Expr(lineNo)}) }
   }
 
