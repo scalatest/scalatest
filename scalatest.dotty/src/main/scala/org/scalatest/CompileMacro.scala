ea8162e3dfb2aa59f9419567a863fb19908b34c6
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-07-08 12:59:56 +0200

    Use quoted.Reporting

--- scalatest.dotty/src/main/scala/org/scalatest/CompileMacro.scala
+++ scalatest.dotty/src/main/scala/org/scalatest/CompileMacro.scala
@@ -197,8 +197,7 @@ object CompileMacro {
         checkNotCompile(code)
 
       case other =>
-        qctx.error("The '" + shouldOrMust + " compile' syntax only works with String literals.")
-        '{???}
+        Reporting.throwError("The '" + shouldOrMust + " compile' syntax only works with String literals.")
     }
   }
 
@@ -250,8 +249,7 @@ object CompileMacro {
         checkNotTypeCheck(code.toString)
 
       case _ =>
-        qctx.error("The '" + shouldOrMust + "Not typeCheck' syntax only works with String literals.")
-        '{???}
+        Reporting.throwError("The '" + shouldOrMust + "Not typeCheck' syntax only works with String literals.")
     }
   }
 
@@ -302,9 +300,7 @@ object CompileMacro {
         checkCompile(code.toString)
 
       case other =>
-        println("###other: " + other)
-        qctx.error("The '" + shouldOrMust + " compile' syntax only works with String literals.")
-        '{???}
+        Reporting.throwError("The '" + shouldOrMust + " compile' syntax only works with String literals.")
     }
   }
 
