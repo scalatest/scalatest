405a762b5748fe7dda7bc5116050f4f2255b1981
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-07-12 13:58:14 +0200

    Remove unnecessary imports

--- common-test.dotty/src/main/scala/org/scalatest/LineNumberHelper.scala
+++ common-test.dotty/src/main/scala/org/scalatest/LineNumberHelper.scala
@@ -1,7 +1,6 @@
 package org.scalatest
 
 import scala.quoted._
-import scala.tasty._
 
 private[scalatest] trait LineNumberHelper {
   inline def thisLineNumber = ${ LineNumberMacro.thisLineNumberImpl }
