d1afdaaae8e8b02f777546b10aa807d5aaeef103
Chua Chee Seng <cheeseng@amaseng.com>
2020-07-27 10:14:45 +0800

    Updated to use scala-js 1.1.1.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ sbt "project scalactic" clean +publishSigned
 export SCALAJS_VERSION=0.6.32
 sbt "project scalacticMacroJS" clean
 sbt "project scalacticJS" clean +publishSigned
-export SCALAJS_VERSION=1.0.1
+export SCALAJS_VERSION=1.1.1
 sbt "project scalacticMacroJS" clean
 sbt ++2.11.12 "project scalacticJS" clean publishSigned
 sbt ++2.12.11 "project scalacticJS" clean publishSigned
@@ -17,7 +17,9 @@ sbt scalatestCompatible/clean scalatestCompatible/publishSigned
 sbt "project scalatest" clean +publishSigned
 export SCALAJS_VERSION=0.6.32
 sbt "project scalatestJS" clean +publishSigned
-export SCALAJS_VERSION=1.0.1
+export SCALAJS_VERSION=1.1.1
+sbt "project scalacticMacroJS" clean
+sbt "project scalacticJS" clean
 sbt ++2.11.12 "project scalatestJS" clean publishSigned
 sbt ++2.12.11 "project scalatestJS" clean publishSigned
 sbt ++2.13.2 "project scalatestJS" clean publishSigned
@@ -28,7 +30,9 @@ sbt clean
 sbt "project scalatestApp" clean +publishSigned
 export SCALAJS_VERSION=0.6.32
 sbt "project scalatestAppJS" clean +publishSigned
-export SCALAJS_VERSION=1.0.1
+export SCALAJS_VERSION=1.1.1
+sbt "project scalacticMacroJS" clean
+sbt "project scalacticJS" clean
 sbt ++2.11.12 "project scalatestAppJS" clean publishSigned
 sbt ++2.12.11 "project scalatestAppJS" clean publishSigned
 sbt ++2.13.2 "project scalatestAppJS" clean publishSigned
