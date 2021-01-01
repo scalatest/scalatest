05b92a74f36fbfd29163032f7cb775c3e7a8a8c1
Chua Chee Seng <cheeseng@amaseng.com>
2021-01-01 17:22:23 +0800

    Enabled ShouldEqualEqualitySpec.scala under Scala 3 build.

--- project/GenScalaTestDotty.scala
+++ project/GenScalaTestDotty.scala
@@ -224,7 +224,6 @@ object GenScalaTestDotty {
         "ShouldBeShorthandSpec.scala", // skipped because does not compile yet 
         "ShouldBeSymbolSpec.scala", // skipped because does not compile yet 
         "ShouldBeThrownBySpec.scala", // skipped because does not compile yet 
-        "ShouldEqualEqualitySpec.scala", // skipped because does not compile yet 
         "ShouldHavePropertiesSpec.scala", // skipped because does not compile yet 
         "ShouldLengthSizeSpec.scala", // skipped because does not compile yet 
         "ShouldLengthSpec.scala", // skipped because does not compile yet 
