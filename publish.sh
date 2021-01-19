66dd16e9f2e930799a9260768de598d3d2fc27de
Ondra Pelech <ondra.pelech@gmail.com>
2021-01-19 16:02:18 +0100

    Scala Native 0.4.0

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ sbt "project scalacticMacroJS" clean
 sbt ++2.11.12 "project scalacticJS" clean publishSigned
 sbt ++2.12.12 "project scalacticJS" clean publishSigned
 sbt ++2.13.4 "project scalacticJS" clean publishSigned
-export SCALANATIVE_VERSION=0.4.0-M2
+export SCALANATIVE_VERSION=0.4.0
 sbt "project scalacticMacroNative" clean
 sbt ++2.11.12 "project scalacticNative" clean publishSigned
 sbt "project scalacticDotty" clean publishSigned
@@ -27,7 +27,7 @@ sbt "project scalacticJS" clean
 sbt ++2.11.12 "project scalatestJS" clean publishSigned
 sbt ++2.12.12 "project scalatestJS" clean publishSigned
 sbt ++2.13.4 "project scalatestJS" clean publishSigned
-export SCALANATIVE_VERSION=0.4.0-M2
+export SCALANATIVE_VERSION=0.4.0
 sbt "project scalacticMacroNative" clean
 sbt ++2.11.12 "project scalatestNative" clean publishSigned
 sbt "project scalatestDotty" clean publishSigned
@@ -45,7 +45,7 @@ sbt "project scalacticJS" clean
 sbt ++2.11.12 "project scalatestAppJS" clean publishSigned
 sbt ++2.12.12 "project scalatestAppJS" clean publishSigned
 sbt ++2.13.4 "project scalatestAppJS" clean publishSigned
-export SCALANATIVE_VERSION=0.4.0-M2
+export SCALANATIVE_VERSION=0.4.0
 sbt "project scalacticMacroNative" clean
 sbt ++2.11.12 "project scalatestAppNative" clean publishSigned
 sbt "project scalatest" sonatypeBundleUpload
