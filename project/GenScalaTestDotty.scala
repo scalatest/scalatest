af82ca3c5e928aecaa9024dd16a247b533dd08aa
Chua Chee Seng <cheeseng@amaseng.com>
2021-03-29 12:00:13 +0800

    Enabled tests under org.scalatest.exceptions package for dotty-js.

--- project/GenScalaTestDotty.scala
+++ project/GenScalaTestDotty.scala
@@ -583,8 +583,8 @@ object GenScalaTestDotty {
         "DeprecatedLocationSuiteProp.scala", // skipped because does not compile yet.
         "DeprecatedLocationFunctionSuiteProp.scala" // skipped because does not compile yet.
       )
-    ) /*++
-    copyDir("jvm/scalatest-test/src/test/scala/org/scalatest/exceptions", "org/scalatest/exceptions", targetDir, List.empty) ++
+    ) ++
+    copyDirJS("jvm/scalatest-test/src/test/scala/org/scalatest/exceptions", "org/scalatest/exceptions", targetDir, List.empty) /*++
     /*copyDir("jvm/scalatest-test/src/test/scala/org/scalatest/fixture", "org/scalatest/fixture", targetDir,
       List(
         "SpecSpec.scala",     // skipped because depends on java reflections
