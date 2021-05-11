d9d201e0054c4c5a7156f6b165e640e3332f411d
Chua Chee Seng <cheeseng@amaseng.com>
2021-05-11 10:15:13 +0800

    Enabled all example test classes under org.scalatest.suiteprop package for Scala 3 JS build.

--- project/GenScalaTestDotty.scala
+++ project/GenScalaTestDotty.scala
@@ -528,23 +528,8 @@ object GenScalaTestDotty {
       )) ++
     copyDirJS("jvm/scalatest-test/src/test/scala/org/scalatest/suiteprop", "org/scalatest/suiteprop", targetDir, 
       List(
-        "DeprecatedFirstTestIgnoredExamples.scala", 
-        "DeprecatedSecondTestIgnoredExamples.scala", 
-        "DeprecatedInfoInsideTestFiredAfterTestExamples.scala", 
-        "DeprecatedTwoSlowTestsExample.scala", 
-        "DeprecatedTwoSlowAndOneWeakTestExamples.scala", 
-        "DeprecatedTwoTestsIgnoredExamples.scala", 
-        "FirstTestIgnoredExamples.scala", 
-        "InfoInsideTestFiredAfterTestExamples.scala", 
         "PathSuiteMatrix.scala", 
-        "PathBeforeAndAfterExamples.scala", 
-        "PathListBufferExamples.scala", 
-        "OnlyFirstTestExecutedOnCreationExamples.scala", 
-        "SecondTestIgnoredExamples.scala", 
         "SuiteMatrix.scala", 
-        "TwoSlowAndOneWeakTestExamples.scala", 
-        "TwoTestsIgnoredExamples.scala", 
-        "TwoSlowTestsExample.scala"
       )
     ) ++
     copyDirJS("jvm/scalatest-test/src/test/scala/org/scalatest/matchers", "org/scalatest/matchers", targetDir, List.empty) ++
