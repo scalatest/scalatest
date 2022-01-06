7dce1c05ce6994f4365586a44fed10d13370c5c1
Chua Chee Seng <cheeseng@amaseng.com>
2022-01-06 22:57:04 +0800

    Enhanced Scala 3 macro to support OrderingOps correctly.

--- dotty/scalactic/src/main/scala/org/scalactic/BooleanMacro.scala
+++ dotty/scalactic/src/main/scala/org/scalactic/BooleanMacro.scala
@@ -99,7 +99,7 @@ object BooleanMacro {
       case Apply(sel @ Select(lhs, op), rhs :: Nil) =>
         def binaryDefault =
           if (isByNameMethodType(sel.tpe)) defaultCase
-          else if (supportedBinaryOperations.contains(op))
+          else if (supportedBinaryOperations.contains(op)) {
             let(Symbol.spliceOwner, lhs) { left =>
               let(Symbol.spliceOwner, rhs) { right =>
                 val app = left.select(sel.symbol).appliedTo(right)
@@ -112,6 +112,7 @@ object BooleanMacro {
                 }
               }
             }.asExprOf[Bool]
+          }
           else defaultCase
 
         op match {
@@ -164,6 +165,26 @@ object BooleanMacro {
               case _ =>
                 binaryDefault
             }
+
+          case ">" | "<" | ">=" | "<=" =>
+            lhs match {
+              case Apply(Apply(TypeApply(Ident(infixOrderingOps), List(_)), List(wrapped)), List(Apply(TypeApply(Ident(_), List(_)), List(TypeApply(Ident(_), List(_)))))) if infixOrderingOps == "infixOrderingOps" =>
+                let(Symbol.spliceOwner, lhs) { left =>
+                  let(Symbol.spliceOwner, rhs) { right =>
+                    val app = left.select(sel.symbol).appliedTo(right)
+                    let(Symbol.spliceOwner, app) { result =>
+                      val l = wrapped.asExpr
+                      val r = right.asExpr
+                      val b = result.asExprOf[Boolean]
+                      val code = '{ Bool.binaryMacroBool($l, ${Expr(op)}, $r, $b, $prettifier) }
+                      code.asTerm
+                    }
+                  }
+                }.asExprOf[Bool]
+                        
+              case _ => binaryDefault
+            }
+
           case "exists" =>
             rhs match {
               case AnonFunction(rhsInner) => // see the assumption for `rhsInner` in `AnonFunction`
