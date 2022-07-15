6a015c36088b721bc4d8ae75bf26735fe682bbf3
Chua Chee Seng <cheeseng@amaseng.com>
2022-07-15 20:12:48 +0800

    Marked more tests in TimeLimitsSpec and JavaFuturesSpec as flicker.

--- jvm/scalatest-test/src/test/scala/org/scalatest/concurrent/TimeLimitsSpec.scala
+++ jvm/scalatest-test/src/test/scala/org/scalatest/concurrent/TimeLimitsSpec.scala
@@ -816,7 +816,7 @@ class TimeLimitsSpec extends AsyncFunSpec with Matchers {
         }
       }
 
-      it("should pass normally when the timeout is not reached in main block that create the future") {
+      it("should pass normally when the timeout is not reached in main block that create the future", Flicker) {
         cancelAfter(Span(200, Millis)) {
           SleepHelper.sleep(100)
           Future.successful(Success("test"))
@@ -825,7 +825,7 @@ class TimeLimitsSpec extends AsyncFunSpec with Matchers {
         }
       }
 
-      it("should pass normally when the timeout is not reached in main block that create the future and in the future itself") {
+      it("should pass normally when the timeout is not reached in main block that create the future and in the future itself", Flicker) {
         cancelAfter(Span(200, Millis)) {
           Future {
             SleepHelper.sleep(100)
