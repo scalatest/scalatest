e0db7cf4e015248212d45cdfc7f509e166fe0f13
Chua Chee Seng <cheeseng@amaseng.com>
2022-07-16 20:47:17 +0800

    Another flicker in EventuallySpec.

--- jvm/scalatest-test/src/test/scala/org/scalatest/concurrent/EventuallySpec.scala
+++ jvm/scalatest-test/src/test/scala/org/scalatest/concurrent/EventuallySpec.scala
@@ -60,7 +60,7 @@ class EventuallySpec extends AsyncFunSpec with Matchers with OptionValues /*with
       result should equal (99)
     }
 
-    it("should invoke the function five times if the by-name throws an exception four times before finally returning normally the fifth time") {
+    it("should invoke the function five times if the by-name throws an exception four times before finally returning normally the fifth time", Flicker) {
 
       var count = 0
       eventually {
