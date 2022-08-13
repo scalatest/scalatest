b4d14aaa5bedc7163810d7e096dad715489fef8b
Chua Chee Seng <cheeseng@amaseng.com>
2022-08-13 16:47:58 +0800

    Enhanced ResultOfGreaterThanOrEqualToComparison.scala to include infix in DSL functions for Scala 3 build.

--- jvm/matchers-core/src/main/scala/org/scalatest/matchers/dsl/ResultOfGreaterThanOrEqualToComparison.scala
+++ jvm/matchers-core/src/main/scala/org/scalatest/matchers/dsl/ResultOfGreaterThanOrEqualToComparison.scala
@@ -43,7 +43,10 @@ final class ResultOfGreaterThanOrEqualToComparison[T : Ordering](val right: T) {
    *                 ^  ... invoked by this be method
    * </pre>
    */
+  //DOTTY-ONLY infix def apply(left: T): Boolean = {
+  // SKIP-DOTTY-START 
   def apply(left: T): Boolean = {
+  // SKIP-DOTTY-END  
     val ordering = implicitly[Ordering[T]]
     ordering.gteq(left, right) // left >= right
   }
