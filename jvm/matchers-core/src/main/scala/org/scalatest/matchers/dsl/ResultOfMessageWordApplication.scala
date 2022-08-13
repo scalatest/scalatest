cfebf90da25c46281da9aebaa51ff3aa5fbd2817
Chua Chee Seng <cheeseng@amaseng.com>
2022-08-13 17:25:11 +0800

    Enhanced ResultOfMessageWordApplication.scala to include infix in DSL functions for Scala 3 build.

--- jvm/matchers-core/src/main/scala/org/scalatest/matchers/dsl/ResultOfMessageWordApplication.scala
+++ jvm/matchers-core/src/main/scala/org/scalatest/matchers/dsl/ResultOfMessageWordApplication.scala
@@ -30,7 +30,10 @@ import org.scalactic.Prettifier
 final class ResultOfMessageWordApplication(val expectedMessage: String) {
 
   // TODO: SCALADOC
+  //DOTTY-ONLY infix def apply[T : Messaging](resultOfOfTypeInvocation: ResultOfOfTypeInvocation[T]): HavePropertyMatcher[T, String] = {
+  // SKIP-DOTTY-START
   def apply[T : Messaging](resultOfOfTypeInvocation: ResultOfOfTypeInvocation[T]): HavePropertyMatcher[T, String] = {
+  // SKIP-DOTTY-END  
     new HavePropertyMatcher[T, String] {
       def apply(t: T): HavePropertyMatchResult[String] = {
         val messaging = implicitly[Messaging[T]]
