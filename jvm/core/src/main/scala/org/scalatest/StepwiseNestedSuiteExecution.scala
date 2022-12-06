89610ea0b438c25733780d9060e040a60a92fe58
Chua Chee Seng <cheeseng@amaseng.com>
2022-12-06 10:48:40 +0800

    Updated scaladoc in StepwiseNestedSuiteExecution.scala about passing down None for distributor field, it no longer true.

--- jvm/core/src/main/scala/org/scalatest/StepwiseNestedSuiteExecution.scala
+++ jvm/core/src/main/scala/org/scalatest/StepwiseNestedSuiteExecution.scala
@@ -28,7 +28,7 @@ import collection.mutable.ListBuffer
  * Trait that causes the nested suites of any suite it is mixed into to be run sequentially even if
  * a <a href="Distributor.html"><code>Distributor</code></a> is passed to <code>runNestedSuites</code>. This trait overrides the 
  * <code>runNestedSuites</code> method and fowards every parameter passed to it to a superclass invocation
- * of <code>runNestedSuites</code>, except it always passes <code>None</code> for the <code>Distributor</code>.
+ * of <code>runNestedSuites</code>, and make the nested suites are run and completed one after one in order.
  * Mix in this trait into any suite whose nested suites need to be run sequentially even with the rest of the
  * run is being executed concurrently.
  */
