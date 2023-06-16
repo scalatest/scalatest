64103f50f0aea4198cf114dfab67e7d4af43a733
Chua Chee Seng <cheeseng@amaseng.com>
2023-06-16 22:02:07 +0800

    Updated scala 2.13.10 to 2.13.11, 2.12.17 to 2.12.18.

--- project/BuildCommons.scala
+++ project/BuildCommons.scala
@@ -10,7 +10,7 @@ import ScalaNativePlugin.autoImport.{nativeLinkStubs, nativeDump}
 trait BuildCommons {
 
   lazy val scalaVersionsSettings: Seq[Setting[_]] = Seq(
-    crossScalaVersions := Seq("2.13.10", "2.12.17", "2.11.12"), 
+    crossScalaVersions := Seq("2.13.11", "2.12.18", "2.11.12"), 
     scalaVersion := crossScalaVersions.value.head,
   )
 
