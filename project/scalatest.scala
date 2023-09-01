14b7dd24b3b1233e3590d41662e18fbaf7a88c83
Chua Chee Seng <cheeseng@amaseng.com>
2023-09-01 15:12:28 +0800

    Removed asm dependency as it seems like is no longer in used.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -147,7 +147,6 @@ object ScalatestBuild extends BuildCommons with DottyBuild with NativeBuild with
       "org.scala-sbt" % "test-interface" % "1.0" % "optional",
       "com.google.inject" % "guice" % "4.0" % "optional",
       "org.apache.ant" % "ant" % "1.10.12" % "optional",
-      "org.ow2.asm" % "asm-all" % "4.1" % "optional",
       commonmark
     )
 
