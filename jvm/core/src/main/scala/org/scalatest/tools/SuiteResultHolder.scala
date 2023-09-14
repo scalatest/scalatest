e32c9e606ccebedb314752ba3bf07edce8148f9d
Chua Chee Seng <cheeseng@amaseng.com>
2023-09-14 16:47:17 +0800

    Removed summary function in SuiteResultHolder.scala which is no longer in used.

--- jvm/core/src/main/scala/org/scalatest/tools/SuiteResultHolder.scala
+++ jvm/core/src/main/scala/org/scalatest/tools/SuiteResultHolder.scala
@@ -26,13 +26,5 @@ private[scalatest] class SuiteResultHolder {
     suiteList += result
   }
   
-  def summary: Summary = {
-    val (succeeded, failed, ignored, pending, canceled, scopesPending) = suiteList.foldLeft((0, 0, 0, 0, 0, 0)) { case ((succeeded, failed, ignored, pending, canceled, scopesPending), r) =>
-      (succeeded + r.testsSucceededCount, failed + r.testsFailedCount, ignored + r.testsIgnoredCount, 
-       pending + r.testsPendingCount, canceled + r.testsCanceledCount, scopesPending + r.scopesPendingCount)
-    }
-    Summary(succeeded, failed, ignored, pending, canceled, suiteList.length, suiteList.filter(!_.isCompleted).length, scopesPending)
-  }
-  
   def totalDuration: Long = suiteList.map(s => if (s.duration.isDefined) s.duration.get else 0).sum
 }
