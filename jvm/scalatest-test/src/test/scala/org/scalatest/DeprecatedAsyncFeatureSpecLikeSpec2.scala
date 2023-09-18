ea6a7aa1fb021ed747ac85314be4cd30975aedf0
Chua Chee Seng <cheeseng@amaseng.com>
2023-09-18 16:27:15 +0800

    Changed remaining tests in scalatest-test that are still using scala.scalajs.concurrent.JSExecutionContext.runNow to use custom test executor.

--- jvm/scalatest-test/src/test/scala/org/scalatest/DeprecatedAsyncFeatureSpecLikeSpec2.scala
+++ jvm/scalatest-test/src/test/scala/org/scalatest/DeprecatedAsyncFeatureSpecLikeSpec2.scala
@@ -26,6 +26,8 @@ import org.scalatest.funspec.AsyncFunSpec
 
 class DeprecatedAsyncFeatureSpecLikeSpec2 extends AsyncFunSpec {
 
+  //SCALATESTJS-ONLY override implicit val executionContext = org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits.global
+
   describe("AsyncFeatureSpecLike") {
 
     it("can be used for tests that return Future under parallel async test execution") {
@@ -854,7 +856,7 @@ class DeprecatedAsyncFeatureSpecLikeSpec2 extends AsyncFunSpec {
         // SKIP-SCALATESTJS,NATIVE-START
         override implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
         // SKIP-SCALATESTJS,NATIVE-END
-        // SCALATESTJS-ONLY override implicit val executionContext = scala.scalajs.concurrent.JSExecutionContext.runNow
+        //SCALATESTJS-ONLY override implicit val executionContext = org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits.global
         val a = 1
         Feature("feature 1") {
           Scenario("scenario A") {
