2caec97955c3ba7ee0003952cf8f73fdf8f5f008
Chua Chee Seng <cheeseng@amaseng.com>
2023-10-16 15:53:20 +0800

    Changed NonEmptySet.scala to use org.scalactic.ColCompatHelper.Iterable and IterableOnce.

--- jvm/scalactic/src/main/scala/org/scalactic/anyvals/NonEmptySet.scala
+++ jvm/scalactic/src/main/scala/org/scalactic/anyvals/NonEmptySet.scala
@@ -16,9 +16,8 @@
 package org.scalactic.anyvals
 
 import scala.annotation.unchecked.{ uncheckedVariance => uV }
-import scala.collection.GenIterable
 import scala.collection.GenSet
-import scala.collection.GenTraversableOnce
+import org.scalactic.ColCompatHelper.{IterableOnce, Iterable}
 import scala.collection.generic.CanBuildFrom
 import scala.collection.mutable.Buffer
 import scala.reflect.ClassTag
@@ -177,11 +176,11 @@ final class NonEmptySet[T] private (val toSet: Set[T]) extends AnyVal {
   // TODO: Have I added these extra ++, etc. methods to Every that take a NonEmptySet?
 
   /**
-    * Returns a new <code>NonEmptySet</code> containing the elements of this <code>NonEmptySet</code> followed by the elements of the passed <code>GenTraversableOnce</code>.
+    * Returns a new <code>NonEmptySet</code> containing the elements of this <code>NonEmptySet</code> followed by the elements of the passed <code>IterableOnce</code>.
     * The element type of the resulting <code>NonEmptySet</code> is the most specific superclass encompassing the element types of this <code>NonEmptySet</code>
-    * and the passed <code>GenTraversableOnce</code>.
+    * and the passed <code>IterableOnce</code>.
     *
-    * @param other the <code>GenTraversableOnce</code> to append
+    * @param other the <code>IterableOnce</code> to append
     * @return a new <code>NonEmptySet</code> that contains all the elements of this <code>NonEmptySet</code> followed by all elements of <code>other</code>.
     */
   def ++(other: org.scalactic.ColCompatHelper.IterableOnce[T]): NonEmptySet[T] =
@@ -342,8 +341,8 @@ final class NonEmptySet[T] private (val toSet: Set[T]) extends AnyVal {
     * formed by the elements of the nested <code>NonEmptySet</code>s.
     *
     * <p>
-    * Note: You cannot use this <code>flatten</code> method on a <code>NonEmptySet</code> that contains a <code>GenTraversableOnce</code>s, because 
-    * if all the nested <code>GenTraversableOnce</code>s were empty, you'd end up with an empty <code>NonEmptySet</code>.
+    * Note: You cannot use this <code>flatten</code> method on a <code>NonEmptySet</code> that contains a <code>IterableOnce</code>s, because 
+    * if all the nested <code>IterableOnce</code>s were empty, you'd end up with an empty <code>NonEmptySet</code>.
     * </p>
     *
     * @tparm B the type of the elements of each nested <code>NonEmptySet</code>
@@ -465,7 +464,7 @@ final class NonEmptySet[T] private (val toSet: Set[T]) extends AnyVal {
     */
   final def head: T = toSet.head
 
-  // Methods like headOption I can't get rid of because of the implicit conversion to GenTraversable.
+  // Methods like headOption I can't get rid of because of the implicit conversion to Iterable.
   // Users can call any of the methods I've left out on a NonEmptySet, and get whatever Set would return
   // for that method call. Eventually I'll probably implement them all to save the implicit conversion.
 
@@ -665,13 +664,13 @@ final class NonEmptySet[T] private (val toSet: Set[T]) extends AnyVal {
   final def reduceRightOption[U >: T](op: (T, U) => U): Option[U] = toSet.reduceRightOption(op)
 
   /**
-    * Checks if the given <code>GenIterable</code> contains the same elements in the same order as this <code>NonEmptySet</code>.
+    * Checks if the given <code>Iterable</code> contains the same elements in the same order as this <code>NonEmptySet</code>.
     *
-    * @param that the <code>GenIterable</code> with which to compare
-    * @return <code>true</code>, if both this <code>NonEmptySet</code> and the given <code>GenIterable</code> contain the same elements
+    * @param that the <code>Iterable</code> with which to compare
+    * @return <code>true</code>, if both this <code>NonEmptySet</code> and the given <code>Iterable</code> contain the same elements
     *     in the same order, <code>false</code> otherwise. 
     */
-  final def sameElements[U >: T](that: GenIterable[U]): Boolean = toSet.sameElements(that)
+  final def sameElements[U >: T](that: Iterable[U]): Boolean = toSet.sameElements(that)
 
   /**
     * Checks if the given <code>Every</code> contains the same elements in the same order as this <code>NonEmptySet</code>.
