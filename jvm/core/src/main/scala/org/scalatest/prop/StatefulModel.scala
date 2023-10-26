93378e9e227dbb9770f3064043116b891a3dde58
Chua Chee Seng <cheeseng@amaseng.com>
2023-10-26 10:58:26 +0800

    Added accumulated commands and results to preCondition and postCondition.

--- jvm/core/src/main/scala/org/scalatest/prop/StatefulModel.scala
+++ jvm/core/src/main/scala/org/scalatest/prop/StatefulModel.scala
@@ -21,9 +21,9 @@ trait StatefulModel[R] {
 
   def command(state: State, gen: Generator[Command]): (Command, Generator[Command])
 
-  def preCondition(state: State, command: Command): Boolean
+  def preCondition(state: State, command: Command, accCmd: IndexedSeq[Command], accRes: IndexedSeq[State]): Boolean
 
-  def postCondition(state: State, command: Command): Boolean
+  def postCondition(oldState: State, newState: State, command: Command, accCmd: IndexedSeq[Command], accRes: IndexedSeq[State]): Boolean
 
   private[scalatest] def indicateSuccess(message: => String): R
 
@@ -38,14 +38,14 @@ trait StatefulModel[R] {
     @tailrec def loop(count: Int, state: State, gen: Generator[Command], accCmd: IndexedSeq[Command], accRes: IndexedSeq[State]): R = {
       if (count > 0) {
         val (cmd, newGen) = command(state, gen)
-        if (preCondition(state, cmd)) {
+        if (preCondition(state, cmd, accCmd, accRes)) {
           val newState = nextState(state, cmd)
           val sutNewState = sut.nextState(state, cmd)
           if (newState != sutNewState) {
             val failureMsg = "SUT returned different state." // TODO: shrink and construct more meaning full message.
             indicateFailure(sde => failureMsg, failureMsg, None, pos)
           }
-          else if (!postCondition(newState, cmd)) {
+          else if (!postCondition(state, newState, cmd, accCmd, accRes)) {
             val failureMsg = "Post condition failed." // TODO: shrink and construct more meaning full message.
             indicateFailure(sde => failureMsg, failureMsg, None, pos)
           }
