fbd1164e6f806f9606c8111a27c4c4774f7f637e
Chua Chee Seng <cheeseng@amaseng.com>
2024-10-04 21:33:34 +0800

    Updated copyright years in scaladoc using updateCopyright.bash .

--- jvm/core/src/main/scala/org/scalatest/prop/RoseTree.scala
+++ jvm/core/src/main/scala/org/scalatest/prop/RoseTree.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2020 Artima, Inc.
+ * Copyright 2001-2024 Artima, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
