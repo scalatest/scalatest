ccb3be54314372124a60ba94bf30e6de973f5130
Chua Chee Seng <cheeseng@amaseng.com>
2025-05-05 10:38:36 +0800

    Generate trait ResourceManagerFixture, use scala.util.Using for Scala >= 2.13, and org.scalactic.Using for Scala 2.11 and 2.12.

--- jvm/scalactic/src/main/scala/org/scalactic/Using.scala
+++ jvm/scalactic/src/main/scala/org/scalactic/Using.scala
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2001-2025 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.scalactic
 
 import scala.util.Try
 import scala.util.control.{ControlThrowable, NonFatal}
 
-/** A utility for performing automatic resource management. It can be used to perform an
+/**
+  * This class is ported from the Scala standard library (https://github.com/scala/scala/blob/v2.13.16/src/library/scala/util/Using.scala), and is used to manage resources for Scala 2.11 and 2.12.
+  * 
+  *A utility for performing automatic resource management. It can be used to perform an
   * operation using resources, after which it releases the resources in reverse order
   * of their creation.
   *
