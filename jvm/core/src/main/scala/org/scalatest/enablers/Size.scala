383686e69581b3f49624b24d24cd2ede0e108786
Chua Chee Seng <cheeseng@amaseng.com>
2025-09-30 20:39:37 +0800

    Refactor implicit def in Size.scala to use given and using for Scala 3 build.

--- jvm/core/src/main/scala/org/scalatest/enablers/Size.scala
+++ jvm/core/src/main/scala/org/scalatest/enablers/Size.scala
@@ -119,18 +119,20 @@ trait Size[T] {
  */
 object Size {
 
-  //DOTTY-ONLY import scala.reflect.Selectable.reflectiveSelectable
-
   /**
    * Enable <code>Size</code> implementation for <code>java.util.Collection</code>
    *
    * @tparam JCOL any subtype of <code>java.util.Collection</code>
    * @return <code>Size[JCOL]</code> that supports <code>java.util.Collection</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfJavaCollection[JCOL <: java.util.Collection[_]]: Size[JCOL] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfJavaCollection[JCOL <: java.util.Collection[_]]: Size[JCOL] = 
     new Size[JCOL] {
       def sizeOf(javaColl: JCOL): Long = javaColl.size
     }
+  //DOTTY-ONLY given [JCOL <: java.util.Collection[_]]: Size[JCOL] = sizeOfJavaCollection
 
   /**
    * Enable <code>Size</code> implementation for <code>java.util.Map</code>
@@ -138,10 +140,14 @@ object Size {
    * @tparam JMAP any subtype of <code>java.util.Map</code>
    * @return <code>Size[JMAP]</code> that supports <code>java.util.Map</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfJavaMap[JMAP <: java.util.Map[_, _]]: Size[JMAP] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfJavaMap[JMAP <: java.util.Map[_, _]]: Size[JMAP] = 
     new Size[JMAP] {
       def sizeOf(javaMap: JMAP): Long = javaMap.size
     }
+  //DOTTY-ONLY given [JMAP <: java.util.Map[_, _]]: Size[JMAP] = sizeOfJavaMap
 
   /**
    * Enable <code>Size</code> implementation for <code>org.scalactic.ColCompatHelper.Iterable</code>
@@ -149,10 +155,14 @@ object Size {
    * @tparam ITR any subtype of <code>org.scalactic.ColCompatHelper.Iterable</code>
    * @return <code>Size[ITR]</code> that supports <code>org.scalactic.ColCompatHelper.Iterable</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfIterable[ITR <: Iterable[_]]: Size[ITR] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfIterable[ITR <: Iterable[_]]: Size[ITR] = 
     new Size[ITR] {
       def sizeOf(itr: ITR): Long = itr.size
     }
+  //DOTTY-ONLY given [ITR <: Iterable[_]]: Size[ITR] = sizeOfIterable
 
   /**
    * Enable <code>Size</code> implementation for <code>Array</code>
@@ -160,20 +170,28 @@ object Size {
    * @tparam E the type of the element in the <code>Array</code>
    * @return <code>Size[Array[E]]</code> that supports <code>Array</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfArray[E]: Size[Array[E]] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfArray[E]: Size[Array[E]] = 
     new Size[Array[E]] {
       def sizeOf(arr: Array[E]): Long = arr.length
     }
+  //DOTTY-ONLY given [E]: Size[Array[E]] = sizeOfArray
 
   /**
    * Enable <code>Size</code> implementation for <code>String</code>
    *
    * @return <code>Size[String]</code> that supports <code>String</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit val sizeOfString: Size[String] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY val sizeOfString: Size[String] = 
     new Size[String] {
       def sizeOf(str: String): Long = str.length
     }
+  //DOTTY-ONLY given Size[String] = sizeOfString
 
   import scala.language.reflectiveCalls
 
@@ -183,10 +201,14 @@ object Size {
    * @tparam T any type with <code>size()</code> method that returns <code>Int</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithSizeMethodForInt[T <: AnyRef { def size(): Int}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithSizeMethodForInt[T <: AnyRef { def size(): Int}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.size()
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithSizeMethodForInt[T <: AnyRef { def size(): Int}]: Size[T] = sizeOfAnyRefWithSizeMethodForInt
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with parameterless <code>size</code> method that returns <code>Int</code>.
@@ -194,10 +216,14 @@ object Size {
    * @tparam T any type with parameterless <code>size</code> method that returns <code>Int</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithParameterlessSizeMethodForInt[T <: AnyRef { def size: Int}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithParameterlessSizeMethodForInt[T <: AnyRef { def size: Int}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.size
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithParameterlessSizeMethodForInt[T <: AnyRef { def size: Int}]: Size[T] = sizeOfAnyRefWithParameterlessSizeMethodForInt  
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with <code>getSize()</code> method that returns <code>Int</code>.
@@ -205,10 +231,14 @@ object Size {
    * @tparam T any type with <code>getSize()</code> method that returns <code>Int</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithGetSizeMethodForInt[T <: AnyRef { def getSize(): Int}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithGetSizeMethodForInt[T <: AnyRef { def getSize(): Int}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.getSize()
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithGetSizeMethodForInt[T <: AnyRef { def getSize(): Int}]: Size[T] = sizeOfAnyRefWithGetSizeMethodForInt  
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with parameterless <code>getSize</code> method that returns <code>Int</code>.
@@ -216,10 +246,14 @@ object Size {
    * @tparam T any type with parameterless <code>getSize</code> method that returns <code>Int</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithParameterlessGetSizeMethodForInt[T <: AnyRef { def getSize: Int}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithParameterlessGetSizeMethodForInt[T <: AnyRef { def getSize: Int}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.getSize
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithParameterlessGetSizeMethodForInt[T <: AnyRef { def getSize: Int}]: Size[T] = sizeOfAnyRefWithParameterlessGetSizeMethodForInt  
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with <code>size()</code> method that returns <code>Long</code>.
@@ -227,10 +261,14 @@ object Size {
    * @tparam T any type with <code>size()</code> method that returns <code>Long</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithSizeMethodForLong[T <: AnyRef { def size(): Long}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithSizeMethodForLong[T <: AnyRef { def size(): Long}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.size()
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithSizeMethodForLong[T <: AnyRef { def size(): Long}]: Size[T] = sizeOfAnyRefWithSizeMethodForLong   
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with parameterless <code>size</code> method that returns <code>Long</code>.
@@ -238,10 +276,14 @@ object Size {
    * @tparam T any type with parameterless <code>size</code> method that returns <code>Long</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithParameterlessSizeMethodForLong[T <: AnyRef { def size: Long}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithParameterlessSizeMethodForLong[T <: AnyRef { def size: Long}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.size
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithParameterlessSizeMethodForLong[T <: AnyRef { def size: Long}]: Size[T] = sizeOfAnyRefWithParameterlessSizeMethodForLong   
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with <code>getSize()</code> method that returns <code>Long</code>.
@@ -249,10 +291,14 @@ object Size {
    * @tparam T any type with <code>getSize()</code> method that returns <code>Long</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithGetSizeMethodForLong[T <: AnyRef { def getSize(): Long}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithGetSizeMethodForLong[T <: AnyRef { def getSize(): Long}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.getSize()
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithGetSizeMethodForLong[T <: AnyRef { def getSize(): Long}]: Size[T] = sizeOfAnyRefWithGetSizeMethodForLong  
 
   /**
    * Enable <code>Size</code> implementation for arbitary object with <code>getSize</code> method that returns <code>Long</code>.
@@ -260,8 +306,12 @@ object Size {
    * @tparam T any type with <code>getSize</code> method that returns <code>Long</code>
    * @return <code>Size[T]</code> that supports <code>T</code> in <code>have size</code> syntax
    */
+  // SKIP-DOTTY-START
   implicit def sizeOfAnyRefWithParameterlessGetSizeMethodForLong[T <: AnyRef { def getSize: Long}]: Size[T] = 
+  // SKIP-DOTTY-END
+  //DOTTY-ONLY def sizeOfAnyRefWithParameterlessGetSizeMethodForLong[T <: AnyRef { def getSize: Long}]: Size[T] = 
     new Size[T] {
       def sizeOf(obj: T): Long = obj.getSize
     }
+  //DOTTY-ONLY given given_sizeOfAnyRefWithParameterlessGetSizeMethodForLong[T <: AnyRef { def getSize: Long}]: Size[T] = sizeOfAnyRefWithParameterlessGetSizeMethodForLong  
 }
